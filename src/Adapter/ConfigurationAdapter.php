904f06d304ac3786c7d2feea1966963bbaff2e68
margud <marius.gudauskis@invertus.eu>
2021-04-27 13:32:17 +0300

    php stan changes

--- src/Adapter/ConfigurationAdapter.php
+++ src/Adapter/ConfigurationAdapter.php
@@ -12,11 +12,25 @@ class ConfigurationAdapter
      */
     private $shopId;
 
+    /**
+     * ConfigurationAdapter constructor.
+     *
+     * @param int $shopId
+     */
     public function __construct($shopId)
     {
         $this->shopId = $shopId;
     }
 
+    /**
+     * @param string $key
+     * @param int|null $idLang
+     * @param int|null $idShopGroup
+     * @param int|null $idShop
+     * @param bool $default
+     *
+     * @return false|string
+     */
     public function get($key, $idLang = null, $idShopGroup = null, $idShop = null, $default = false)
     {
         if ($idShop === null) {
@@ -26,6 +40,15 @@ class ConfigurationAdapter
         return Configuration::get($key, $idLang, $idShopGroup, $idShop, $default);
     }
 
+    /**
+     * @param string $key
+     * @param string|int $values
+     * @param bool $html
+     * @param int|null $idShopGroup
+     * @param int|null $idShop
+     *
+     * @return bool
+     */
     public function updateValue($key, $values, $html = false, $idShopGroup = null, $idShop = null)
     {
         if ($idShop === null) {
@@ -35,6 +58,11 @@ class ConfigurationAdapter
         return Configuration::updateValue($key, $values, $html, $idShopGroup, $idShop);
     }
 
+    /**
+     * @param string $key
+     *
+     * @return bool
+     */
     public function deleteByName($key)
     {
         return Configuration::deleteByName($key);
