8dc33174f33f017ef7b62fe64ce70fec14f87e5e
margud <marius.gudauskis@invertus.eu>
2021-04-28 10:33:22 +0300

    removed not needed code

--- controllers/front/apiShipping.php
+++ controllers/front/apiShipping.php
@@ -1,9 +1,6 @@
 <?php
 
-use PrestaShop\Module\PsEventbus\Builder\CarrierBuilder;
 use PrestaShop\Module\PsEventbus\Controller\AbstractApiController;
-use PrestaShop\Module\PsEventbus\DTO\Carrier as EventBusCarrier;
-use PrestaShop\Module\PsEventbus\Exception\EnvVarException;
 use PrestaShop\Module\PsEventbus\Provider\ShippingDataProvider;
 
 class ps_EventbusApiShippingModuleFrontController extends AbstractApiController
@@ -22,43 +19,5 @@ class ps_EventbusApiShippingModuleFrontController extends AbstractApiController
         $response = $this->handleDataSync($categoryDataProvider);
 
         $this->exitWithResponse($response);
-
-        $response = [];
-
-        $language = new Language(Configuration::get('PS_LANG_DEFAULT'));
-        $currency = new Currency(Configuration::get('PS_CURRENCY_DEFAULT'));
-
-        $jobId = Tools::getValue('job_id');
-
-        /** @var CarrierBuilder $carrierBuilder */
-        $carrierBuilder = $this->module->getService(CarrierBuilder::class);
-
-        $carriers = Carrier::getCarriers($language->id);
-
-        /** @var EventBusCarrier[] $eventBusCarriers */
-        $eventBusCarriers = $carrierBuilder->buildCarriers(
-            $carriers,
-            $language,
-            $currency,
-            Configuration::get('PS_WEIGHT_UNIT')
-        );
-
-        try {
-            $response = $this->proxyService->upload($jobId, $eventBusCarriers, $this->startTime);
-        } catch (EnvVarException $exception) {
-            $this->exitWithExceptionMessage($exception);
-        } catch (Exception $exception) {
-            $this->exitWithExceptionMessage($exception);
-        }
-
-        $this->exitWithResponse(
-            array_merge(
-                [
-                    'remaining_objects' => 0,
-                    'total_objects' => 1,
-                ],
-                $response
-            )
-        );
     }
 }
