e6597a78dc16fee1bd328ace47d22404c6929d32
margud <marius.gudauskis@invertus.eu>
2021-12-07 16:17:06 +0200

    added sentry for all exceptions for now

--- src/Handler/ErrorHandler/ErrorHandler.php
+++ src/Handler/ErrorHandler/ErrorHandler.php
@@ -54,6 +54,8 @@ class ErrorHandler implements ErrorHandlerInterface
         );
 
         $this->client->setAppPath(realpath(_PS_MODULE_DIR_ . $module->name . '/'));
+        $this->client->install();
+
     }
 
     /**
