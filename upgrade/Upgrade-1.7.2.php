3679289ef6a8fac646ad552e68bc982d13625f68
margud <marius.gudauskis@invertus.eu>
2022-04-20 17:50:57 +0300

    upgrade fix

--- upgrade/Upgrade-1.7.2.php
+++ upgrade/Upgrade-1.7.2.php
@@ -9,9 +9,9 @@ function upgrade_module_1_7_2()
 {
     $db = Db::getInstance();
 
-    $db->update('ps_eventbus_incremental_sync', ['type' => Config::COLLECTION_CUSTOM_PRODUCT_CARRIERS], '`type` = "custom_product_carrier"');
-    $db->update('ps_eventbus_incremental_sync', ['type' => Config::COLLECTION_SPECIFIC_PRICES], '`type` = "specific_price"');
-    $db->update('ps_eventbus_incremental_sync', ['type' => Config::COLLECTION_CARRIERS], '`type` = "carrier"');
+    $db->update('eventbus_incremental_sync', ['type' => Config::COLLECTION_CUSTOM_PRODUCT_CARRIERS], '`type` = "custom_product_carrier"');
+    $db->update('eventbus_incremental_sync', ['type' => Config::COLLECTION_SPECIFIC_PRICES], '`type` = "specific_price"');
+    $db->update('eventbus_incremental_sync', ['type' => Config::COLLECTION_CARRIERS], '`type` = "carrier"');
 
     return true;
 }
