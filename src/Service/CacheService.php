4322b9f4932034570042cafc20b364cb24aaca88
Frédéric Comte <frederic.comte@prestashop.com>
2022-10-07 16:25:09 +0200

    generic cache service

--- src/Service/CacheService.php
+++ src/Service/CacheService.php
@@ -15,9 +15,9 @@ class CacheService
     public function setCacheProperty($key, $value)
     {
         $cache = new FilesystemAdapter();
-        $merchantConsentCache = $cache->getItem($key);
-        $merchantConsentCache->set($value);
-        $cache->save($merchantConsentCache);
+        $cacheItem = $cache->getItem($key);
+        $cacheItem->set($value);
+        $cache->save($cacheItem);
     }
 
     /**
@@ -28,8 +28,8 @@ class CacheService
     public function getCacheProperty($key)
     {
         $cache = new FilesystemAdapter();
-        $merchantConsentCache = $cache->getItem($key);
+        $cacheItem = $cache->getItem($key);
 
-        return $merchantConsentCache->get();
+        return $cacheItem->get();
     }
 }
