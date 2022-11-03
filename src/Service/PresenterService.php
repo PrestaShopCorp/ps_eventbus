ebf9e216012be4bcc8a59a922ce4e3cfa29c7be8
Mikatux <mickael@mayeur.eu>
2022-11-03 16:24:40 +0100

    Update presenter with module, shop and psEventbus info

--- src/Service/PresenterService.php
+++ src/Service/PresenterService.php
@@ -2,39 +2,104 @@
 
 namespace PrestaShop\Module\PsEventbus\Service;
 
+use Configuration;
+use ModuleCore;
 use PrestaShop\AccountsAuth\Service\PsAccountsService;
+use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManagerBuilder;
+use Tools;
 
 class PresenterService
 {
     /**
-     * @var PsAccountsService
+     * @var PsAccountsService|null
      */
-    private $psAccountsService;
+    private $psAccountsService = null;
 
     public function __construct()
     {
-        $accountsModule = \Module::getInstanceByName('ps_accounts');
-        /* @phpstan-ignore-next-line */
-        $accountService = $accountsModule->getService('PrestaShop\Module\PsAccounts\Service\PsAccountsService');
-        $this->psAccountsService = $accountService;
+        $moduleManager = ModuleManagerBuilder::getInstance();
+        if (!$moduleManager) {
+            return;
+        }
+        $moduleManager = $moduleManager->build();
+        if ($moduleManager->isInstalled('ps_accounts')) {
+            $accountsModule = \Module::getInstanceByName('ps_accounts');
+            /* @phpstan-ignore-next-line */
+            $accountService = $accountsModule->getService('PrestaShop\Module\PsAccounts\Service\PsAccountsService');
+            $this->psAccountsService = $accountService;
+        } else {
+            $this->init();
+        }
     }
 
     /**
-     * @param string $moduleName
+     * @return void
+     */
+    public function init()
+    {
+        $moduleManager = ModuleManagerBuilder::getInstance();
+        if (!$moduleManager) {
+            return;
+        }
+        $moduleManager = $moduleManager->build();
+
+        if (!$moduleManager->isInstalled('ps_accounts')) {
+            $moduleManager->install('ps_accounts');
+        } elseif (!$moduleManager->isEnabled('ps_accounts')) {
+            $moduleManager->enable('ps_accounts');
+        } else {
+            $moduleManager->upgrade('ps_accounts');
+        }
+    }
+
+    /**
+     * @param object|ModuleCore|false $object
+     *
+     * @return array
+     */
+    private function convertObjectToArray($object)
+    {
+        if ($object == false) {
+            return [];
+        }
+        $array = [];
+        foreach ($object as $key => $value) {
+            if (is_object($value)) {
+                $array[$key] = $this->convertObjectToArray($value);
+            } else {
+                $array[$key] = $value;
+            }
+        }
+
+        return $array;
+    }
+
+    /**
+     * @param ModuleCore $module
      * @param array $requiredConsents
      * @param array $optionalConsents
      *
      * @return array
      */
-    public function expose($moduleName, $requiredConsents = [], $optionalConsents = [])
+    public function expose($module, $requiredConsents = [], $optionalConsents = [])
     {
-        return [
-            'jwt' => $this->psAccountsService->getOrRefreshToken(),
-            /* @phpstan-ignore-next-line */
-            'shopId' => $this->psAccountsService->getShopUuid(),
-            'requiredConsents' => $requiredConsents,
-            'optionalConsents' => $optionalConsents,
-            'moduleName' => $moduleName,
-        ];
+        if ($this->psAccountsService == null) {
+            return [];
+        } else {
+            return [
+                'jwt' => $this->psAccountsService->getOrRefreshToken(),
+                'requiredConsents' => $requiredConsents,
+                'optionalConsents' => $optionalConsents,
+                'module' => array_merge([
+                    'logoUrl' => Tools::getHttpHost(true) . '/modules/' . $module->name . '/logo.png',
+                ], $this->convertObjectToArray($module)),
+                'shop' => [
+                    /* @phpstan-ignore-next-line */
+                    'id' => $this->psAccountsService->getShopUuid(),
+                    'name' => Configuration::get('PS_SHOP_NAME'),
+                ],
+                'psEventbusModule' => $this->convertObjectToArray(\Module::getInstanceByName('ps_eventbus')),
+            ];
+        }
     }
 }
