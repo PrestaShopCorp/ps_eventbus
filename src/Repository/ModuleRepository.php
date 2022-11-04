a4c8dbfc5abc994d8820b805b31ed8afa4fe7314
synicko <nicolas.charpin@synako.fr>
2022-11-04 15:48:32 +0100

    Correction data source pour module.active

--- src/Repository/ModuleRepository.php
+++ src/Repository/ModuleRepository.php
@@ -9,6 +9,7 @@ class ModuleRepository
 {
     public const MODULE_TABLE = 'module';
     public const MODULE_TABLE_HISTORY = 'module_history';
+    public const MODULE_SHOP = 'module_shop';
 
     /**
      * @var Db
@@ -27,7 +28,8 @@ class ModuleRepository
     {
         return (new DbQuery())
           ->from(self::MODULE_TABLE, 'm')
-          ->leftJoin(self::MODULE_TABLE_HISTORY, 'h', 'm.id_module = h.id_module');
+          ->leftJoin(self::MODULE_TABLE_HISTORY, 'h', 'm.id_module = h.id_module')
+          ->leftJoin(self::MODULE_SHOP, 'm_shop', 'm.id_module = m_shop.id_module');
     }
 
     /**
@@ -42,7 +44,7 @@ class ModuleRepository
     {
         $query = $this->getBaseQuery();
 
-        $query->select('m.id_module as module_id, name, version as module_version, active, date_add as created_at, date_upd as updated_at')
+        $query->select('m.id_module as module_id, name, version as module_version, IF(m_shop.enable_device, 1, 0) as active, date_add as created_at, date_upd as updated_at')
             ->limit($limit, $offset);
 
         return $this->db->executeS($query);
