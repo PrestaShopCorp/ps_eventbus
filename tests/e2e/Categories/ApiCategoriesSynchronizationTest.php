b056f3a4b72ea3163a841bd7fd3ffb5bdb3e2d36
Guillaume Lepoetre <guillaume.lepoetre@prestashop.com>
2023-02-17 09:50:02 +0100

    add phpmyadmin

---
+++ tests/e2e/Categories/ApiCategoriesSynchronizationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\e2e\Categories;
+
+use PrestaShop\Module\PsEventbus\Tests\System\Tests\BaseTestCase;
+
+class CategoriesSyncTest extends BaseTestCase
+{
+    public function testCategoriesSync()
+    {
+        echo "Not implement yet";
+
+        return 'Not implement yet';
+    }
+}
+
