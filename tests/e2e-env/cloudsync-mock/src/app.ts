4bf8a4381d877d893fafdb91e53b71b033229831
Guillaume Lepoetre <guillaume.lepoetre@prestashop.com>
2023-02-22 17:44:42 +0100

    create new container to run test on docker

--- tests/e2e-env/cloudsync-mock/src/app.ts
+++ tests/e2e-env/cloudsync-mock/src/app.ts
@@ -1,42 +1,46 @@
 import fetch from "node-fetch";
 import express from "express";
 
-import request from "supertest";
-import assert from "assert";
-
-
 /*********** SYNC API MOCK**********************/
-async function createSyncApi(){
+const SYNC_API_PORT = 3232;
 
-    const SYNC_API_PORT = 3232;
-    
-    const syncApi = express();
-    syncApi.listen(SYNC_API_PORT, () => {
-        console.log(`sync-api running on :${SYNC_API_PORT}`);
-    });
-    syncApi.get('/job/:jobId', function(req, res) {
-        console.log(`request to sync-api jobid = ${req.params.jobId}`);
-        res.status(201).end()
-    });
-}
+const syncApi = express();
+syncApi.listen(SYNC_API_PORT, () => {
+    console.log(`sync-api running on :${SYNC_API_PORT}`);
+});
+syncApi.get('/job/:id', function(req, res) {
+    console.log(`request to sync-api jobid = ${req.params.id}`)
+    switch (req.params.id) {
+        case 'valid-job-id':
+            res.status(200).end()
+            break;
+        case 'invalid-job-id':
+            res.status(500).end()
+            break;
+    }       
+});
 
 /*********** PROXY API MOCK **********************/
-async function createProxyApi(){
+const PROXY_API_PORT = 3333;
 
-    const PROXY_API_PORT = 3333;
-    
-    const proxyApi = express();
-    proxyApi.listen(PROXY_API_PORT, () => {
-        console.log(`proxy-api running on ${PROXY_API_PORT}`);
-    });
-    proxyApi.post('/upload/:jobId', function(req, res) {
-        console.log(`request to proxy-api jobid = ${req.params.jobId}`)
-        res.status(201).end()
-    });
-}
+const proxyApi =  express();
+proxyApi.listen(PROXY_API_PORT, () => {
+    console.log(`proxy-api running on ${PROXY_API_PORT}`);
+});
+proxyApi.post('/upload/:job_id', function(req, res) {
+    console.log(`request to proxy-api jobid = ${req.params.job_id}`)
+    switch (req.params.job_id) {
+        case 'valid-job-id':
+            res.status(200).end()
+            break;
+        case 'invalid-job-id':
+            res.status(500).end()
+            break;
+    }       
+});
 
-async function getCategories() {
-    const response = await fetch('http://prestashop/index.php?fc=module&module=ps_eventbus&controller=apiCategories&limit=5&job_id=12', {
+/* async function getCategories() {
+    const response = await fetch('http://prestashop/index.php?fc=module&module=ps_eventbus&controller=apiCategories&limit=5&job_id=valid-job-id', {
         method: 'GET',
     });
     console.log(response);
@@ -45,49 +49,10 @@ async function getCategories() {
 
 async function getHealthCheck() {
     console.log('getHealthCheck')
-    const response =  await fetch('http://prestashop/index.php?fc=module&module=ps_eventbus&controller=apiHealthCheck')
-    return await response.json();
-}
-
-async function checkServer() {
-    const syncApi =  await fetch('http://localhost:3232/job/12', {
+    const response =  await fetch('http://prestashop/index.php?fc=module&module=ps_eventbus&controller=apiHealthCheck', {
         method: 'GET',
     })
-    console.log('SyncApi => ', syncApi);
-    const proxyApi =  await fetch('http://localhost:3333/upload/12', {
-        method: 'POST',
-    })
-    console.log('ProxyApi => ', proxyApi);
-
-}
-
-
-createSyncApi().then(() => {
-    createProxyApi().then(() => {
-        checkServer().then( async() => {
-            const request = await getCategories()
-            console.log(request);
-        });
-    });
-});
-
-/* const healcheck = getHealthCheck().then((response) => {
-    console.log(response);
-    return response;
-}); */
-
-/* const categories =  getCategories().then((response) => {
-    console.log(response);
-    return response;
-}); */
-
-/* request(app)
-    .get('http://localhost:8000/index.php?fc=module&module=ps_eventbus&controller=apiCategories&job_id=42&limit=5')
-    .expect('Content-Type', /json/)
-    .expect('Content-Length', '15')
-    .expect(200)
-    .end(function(err, res) {
-      if (err) throw err;
-}); */
+    return await response.json();
+} */
 
 export class App{};
