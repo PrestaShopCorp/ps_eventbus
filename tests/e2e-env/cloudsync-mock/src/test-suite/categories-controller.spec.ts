e7f40fa03bca21ec33ea0c273e3b03851016a7ac
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2023-06-21 14:50:29 +0200

    Add more notes

--- tests/e2e-env/cloudsync-mock/src/test-suite/categories-controller.spec.ts
+++ tests/e2e-env/cloudsync-mock/src/test-suite/categories-controller.spec.ts
@@ -11,15 +11,15 @@ import request from "supertest";
 import { ProxyApi, SyncApi } from "../helpers/api-mock";
 import configTest from "./config.test";
 
-const controller = "apiCategories";
-
-const url = configTest.prestashopUrl;
-const endpoint = `/index.php?fc=module&module=ps_eventbus&controller=${controller}&limit=5`;
 describe("CategoriesController", () => {
   let syncApi;
   let syncApiRequestData;
   let proxyApi;
   let proxyApiRequestData;
+
+  const url = configTest.prestashopUrl;
+  const controller = "apiCategories";
+  const endpoint = `/index.php?fc=module&module=ps_eventbus&controller=${controller}&limit=5`;
 
   beforeAll(async () => {
     syncApi = new SyncApi(3232);
@@ -28,30 +28,40 @@ describe("CategoriesController", () => {
     proxyApi = new ProxyApi(3333);
     proxyApiRequestData = jest.spyOn(proxyApi, "requestData");
   });
+
   afterEach(() => {
     syncApiRequestData.mockClear();
     proxyApiRequestData.mockClear();
   });
 
+  afterAll(() => {
+    syncApi.close();
+    proxyApi.close();
+  });
+
   it("should be defined", () => {
     expect(url).toBeDefined();
   });
+
   it("should return 500 because job is invalid (sync-api response == 500)", async () => {
-    jest.setTimeout(10000);
+    jest.setTimeout(10_000); // TODO: timeouts should not be needed
     const id = Date.now();
+
     await request(url)
       .get(`${endpoint}&job_id=invalid-job-${id}}`)
       .redirects(1)
       .expect("content-type", /json/)
-      .expect(500);
+      .expect(500); // @TODO: why 500 and not 403/401?
 
     expect(syncApiRequestData).toHaveBeenCalledWith(
       expect.objectContaining({
         url: expect.stringContaining(`invalid-job-${id}`),
       })
     );
+
     expect(proxyApiRequestData).not.toHaveBeenCalled();
   });
+
   it("should synchronize (sync-api and proxy-api response == 201)", async () => {
     const jobId = Date.now();
     const req = await request(url)
@@ -59,26 +69,27 @@ describe("CategoriesController", () => {
       .redirects(1)
       .expect("content-type", /json/)
       .expect(201);
+    // @TODO check req body output
+    // @TODO job ids do have a local cache
 
     expect(syncApiRequestData).toHaveBeenCalledWith(
       expect.objectContaining({
         url: expect.stringContaining(`valid-job-${jobId}`),
       })
     );
+
     expect(proxyApiRequestData).toHaveBeenCalledWith(
       expect.objectContaining({
         url: expect.stringContaining(`valid-job-${jobId}`),
         files: expect.arrayContaining([
+          // TODO: check content of this file
           expect.objectContaining({ fieldname: "file" }),
         ]),
       })
     );
+
+    // TODO: check the incremental_sync table state
     expect(req.body.total_objects).toEqual(1);
     console.log(req.body);
-  });
-
-  afterAll(() => {
-    syncApi.close();
-    proxyApi.close();
   });
 });
