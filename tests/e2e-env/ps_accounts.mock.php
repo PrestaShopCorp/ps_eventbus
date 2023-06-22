bed63dc3c329658137439218e169f0f2f10b4ea8
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2023-06-22 14:10:04 +0200

    test this

---
+++ tests/e2e-env/ps_accounts.mock.php
@@ -0,0 +1,9 @@
+<?php
+class Ps_accounts_mock {
+  // $this->jwt =  $psAccounts->getPsAccountsService()->getOrRefreshToken();
+  // $this->psAccountsService = $this->module->getService(PsAccounts::class)->getPsAccountsService();
+  // $token = $this->psAccountsService->getOrRefreshToken();
+  // $this->client->set_user_data($accountsService->getPsAccountsService()->getShopUuid(), $configurationPsShopEmail);
+  // $accountsService->getPsAccountsService()->getShopUuid(),
+}
+?>
