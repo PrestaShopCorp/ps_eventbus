c086a504fd651dfd77ac9453369a8545453a9ab0
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2023-06-22 14:21:44 +0200

    Attempt

--- tests/e2e-env/ps_accounts.mock.php
+++ tests/e2e-env/ps_accounts.mock.php
@@ -1,9 +1,33 @@
 <?php
-class Ps_accounts_mock {
-  // $this->jwt =  $psAccounts->getPsAccountsService()->getOrRefreshToken();
-  // $this->psAccountsService = $this->module->getService(PsAccounts::class)->getPsAccountsService();
-  // $token = $this->psAccountsService->getOrRefreshToken();
-  // $this->client->set_user_data($accountsService->getPsAccountsService()->getShopUuid(), $configurationPsShopEmail);
-  // $accountsService->getPsAccountsService()->getShopUuid(),
+class PsAccountsServiceMock
+{
+  /**
+   * Mock a refresh token getter/refresher
+   *
+   * @return string
+   *
+   * @throws \Exception
+   */
+  public function getOrRefreshToken() {
+    $token = 'coucou';
+    return (string) $token;
+  }
+
+  /**
+   * Mock a shopId getter
+   * @return string|false
+   */
+  public function getShopUuid() {
+    $shopId = '00000000-0000-0000-0000-000000000000';
+    return $shopId;
+  }
+}
+
+class Ps_accounts_mock
+{
+  public function getPsAccountsService ()
+  {
+    return new PsAccountsServiceMock();
+  }
 }
 ?>
