7df955616738f5578fda65e39d6af38ad35d5150
John.R <jonathan_renard@hotmail.fr>
2023-10-31 10:45:20 +0100

    fix: remove double quotes

--- tests/Mocks/apply-ps-accounts-mock.sh
+++ tests/Mocks/apply-ps-accounts-mock.sh
@@ -16,11 +16,11 @@ cp ./PSAccounts.php "$SRC_DIR/Mock/PsAccounts.php"
 
 # I could not just inject my PsAccounts mock with the "class" attribute in Yaml
 # so dumb me thought: replace the naming everywhere it will work
-find "$CONFIG_DIR" -type f -exec sed "$SEDOPTION" -e 's/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/g' {} \;
-find "$SRC_DIR" -type f -exec sed "$SEDOPTION" -e 's/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/g' {} \;
+find "$CONFIG_DIR" -type f -exec sed $SEDOPTION -e 's/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/g' {} \;
+find "$SRC_DIR" -type f -exec sed $SEDOPTION -e 's/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/g' {} \;
 
 # Cheating here, I don't know how to mock the following:
 #   $module = \Module::getInstanceByName('ps_accounts');
 #   return $module->getService(AccountsClient::class);
 # shellcheck disable=SC2016
-sed "$SEDOPTION" -e 's/=> $tokenValid/=> true/' "$SRC_DIR/Repository/ServerInformationRepository.php"
+sed $SEDOPTION -e 's/=> $tokenValid/=> true/' "$SRC_DIR/Repository/ServerInformationRepository.php"
--- tests/Mocks/revert-ps-accounts-mock.sh
+++ tests/Mocks/revert-ps-accounts-mock.sh
@@ -13,8 +13,8 @@ fi
 
 rm -rf "$SRC_DIR/Mock"
 
-find "$CONFIG_DIR" -type f -exec sed "$SEDOPTION" -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
-find "$SRC_DIR" -type f -exec sed "$SEDOPTION" -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
+find "$CONFIG_DIR" -type f -exec sed $SEDOPTION -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
+find "$SRC_DIR" -type f -exec sed $SEDOPTION -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
 
 # shellcheck disable=SC2016
-sed "$SEDOPTION" -e 's/=> true/=> $tokenValid/' "$SRC_DIR/Repository/ServerInformationRepository.php"
+sed $SEDOPTION -e 's/=> true/=> $tokenValid/' "$SRC_DIR/Repository/ServerInformationRepository.php"
