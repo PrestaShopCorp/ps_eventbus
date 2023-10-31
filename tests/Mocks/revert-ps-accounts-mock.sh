2fccb36357e34b92d7236e9efcaa320159b61e7f
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2023-10-31 11:03:30 +0100

    Fix for OSX

--- tests/Mocks/revert-ps-accounts-mock.sh
+++ tests/Mocks/revert-ps-accounts-mock.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-cd "$(dirname $0)"
+cd "$(dirname "$0")" || exit 1
 
 SRC_DIR="../../src"
 CONFIG_DIR="../../config"
 
-SEDOPTION=
+SED_PARAMS=()
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    SEDOPTION="-i ''"
+  SED_PARAMS+=(-i '')
 else
-    SEDOPTION="-i"
+  SED_PARAMS+=(-i)
 fi
 
 rm -rf "$SRC_DIR/Mock"
 
-find "$CONFIG_DIR" -type f -exec sed $SEDOPTION -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
-find "$SRC_DIR" -type f -exec sed $SEDOPTION -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
+find "$CONFIG_DIR" -type f -exec sed "${SED_PARAMS[@]}" -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
+find "$SRC_DIR" -type f -exec sed "${SED_PARAMS[@]}" -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
 
 # shellcheck disable=SC2016
-sed $SEDOPTION -e 's/=> true/=> $tokenValid/' "$SRC_DIR/Repository/ServerInformationRepository.php"
+sed "${SED_PARAMS[@]}" -e 's/=> true/=> $tokenValid/' "$SRC_DIR/Repository/ServerInformationRepository.php"
