787fa99bc12e8ce77c23d9716764f811b930aa65
Jonathan Renard <1273438+fox-john@users.noreply.github.com>
2023-10-31 11:05:55 +0100

    fix: revert sh file (#194)
    Co-authored-by: Clément Désiles <1536672+jokesterfr@users.noreply.github.com>

--- tests/Mocks/apply-ps-accounts-mock.sh
+++ tests/Mocks/apply-ps-accounts-mock.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cd "$(dirname $0)"
+cd "$(dirname "$0")" || exit 1
 
 SRC_DIR="../../src"
 CONFIG_DIR="../../config"
 
-SEDOPTION=
+SED_PARAMS=()
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    SEDOPTION="-i ''"
+  SED_PARAMS+=(-i '')
 else
-    SEDOPTION="-i"
+  SED_PARAMS+=(-i)
 fi
 
 mkdir -p "$SRC_DIR/Mock"
@@ -16,11 +16,11 @@ cp ./PSAccounts.php "$SRC_DIR/Mock/PsAccounts.php"
 
 # I could not just inject my PsAccounts mock with the "class" attribute in Yaml
 # so dumb me thought: replace the naming everywhere it will work
-find "$CONFIG_DIR" -type f -exec sed "$SEDOPTION" -e 's/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/g' {} \;
-find "$SRC_DIR" -type f -exec sed "$SEDOPTION" -e 's/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/g' {} \;
+find "$CONFIG_DIR" -type f -exec sed "${SED_PARAMS[@]}" -e 's/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/g' {} \;
+find "$SRC_DIR" -type f -exec sed "${SED_PARAMS[@]}" -e 's/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/g' {} \;
 
 # Cheating here, I don't know how to mock the following:
 #   $module = \Module::getInstanceByName('ps_accounts');
 #   return $module->getService(AccountsClient::class);
 # shellcheck disable=SC2016
-sed "$SEDOPTION" -e 's/=> $tokenValid/=> true/' "$SRC_DIR/Repository/ServerInformationRepository.php"
+sed "${SED_PARAMS[@]}" -e 's/=> $tokenValid/=> true/' "$SRC_DIR/Repository/ServerInformationRepository.php"
