75938ff9a34c3370337251a5e92815f9c13d95cf
John.R <jonathan_renard@hotmail.fr>
2023-10-31 10:32:25 +0100

    fix: revert sh file

--- tests/Mocks/revert-ps-accounts-mock.sh
+++ tests/Mocks/revert-ps-accounts-mock.sh
@@ -4,10 +4,17 @@ cd "$(dirname $0)"
 SRC_DIR="../../src"
 CONFIG_DIR="../../config"
 
+SEDOPTION=
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SEDOPTION="-i ''"
+else
+    SEDOPTION="-i"
+fi
+
 rm -rf "$SRC_DIR/Mock"
 
-find "$CONFIG_DIR" -type f -exec sed -i '' -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
-find "$SRC_DIR" -type f -exec sed -i '' -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
+find "$CONFIG_DIR" -type f -exec "$SEDOPTION" -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
+find "$SRC_DIR" -type f -exec "$SEDOPTION" -e 's/PrestaShop\\Module\\PsEventbus\\Mock\\PsAccounts/PrestaShop\\PsAccountsInstaller\\Installer\\Facade\\PsAccounts/g' {} \;
 
 # shellcheck disable=SC2016
-sed -i '' -e 's/=> true/=> $tokenValid/' "$SRC_DIR/Repository/ServerInformationRepository.php"
+sed "$SEDOPTION" -e 's/=> true/=> $tokenValid/' "$SRC_DIR/Repository/ServerInformationRepository.php"
