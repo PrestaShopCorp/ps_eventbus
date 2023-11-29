62fb3339ad888d0d532434f6f714825d473557a9
John.R <jonathan_renard@hotmail.fr>
2023-11-29 17:05:15 +0100

    feat: add debounce

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -2,3 +2,4 @@ DROP TABLE IF EXISTS `PREFIX_eventbus_type_sync`;
 DROP TABLE IF EXISTS `PREFIX_eventbus_job`;
 DROP TABLE IF EXISTS `PREFIX_eventbus_deleted_objects`;
 DROP TABLE IF EXISTS `PREFIX_eventbus_incremental_sync`;
+DROP TABLE IF EXISTS `PREFIX_eventbus_live_sync`;
