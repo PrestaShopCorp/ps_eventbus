c9cbfa2b1165fa096a1be87d3e6ec794dad629e5
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2023-11-29 21:18:27 +0100

    Mock it FTW

--- tests/e2e-env/init-scripts/install-module.sh
+++ tests/e2e-env/init-scripts/install-module.sh
@@ -4,6 +4,15 @@
 set -eu
 
 cd "$PS_FOLDER"
+echo "* [ps_eventbus] cleaning tools/vendor..."
 rm -rf "./modules/ps_eventbus/tools/vendor"
+echo "* [ps_eventbus] installing the module..."
 php -d memory_limit=-1 bin/console prestashop:module --no-interaction install "ps_eventbus"
-                                                   
+
+cp -r /tmp/init-scripts/ps_accounts ./modules/
+cd ./modules/ps_accounts
+echo "* [ps_accounts_mock] composer build..."
+composer dump-autoload;
+echo "* [ps_accounts_mock] installing the module..."
+cd "$PS_FOLDER"
+php -d memory_limit=-1 bin/console prestashop:module --no-interaction install "ps_accounts"
---
+++ tests/e2e-env/init-scripts/ps_accounts/ps_accounts.php
@@ -0,0 +1,69 @@
+<?php
+if (!defined('_PS_VERSION_'))
+{
+  exit;
+}
+
+class Ps_accounts extends Module
+{
+  
+  /**
+   * @var string
+   */
+  const VERSION = '0.0.0';
+
+  /**
+   * @var \PrestaShop\ModuleLibServiceContainer\DependencyInjection\ServiceContainer
+   */
+  private $serviceContainer;
+
+  public function __construct()
+  {
+
+    $this->name = 'ps_accounts';
+    $this->version = '42.0.0';
+    $this->author = 'CloudSync team';
+    $this->need_instance = 0;
+    $this->ps_versions_compliancy = [
+      'min' => '1.6.1.24',
+      'max' => '99.99.99',
+    ];
+    $this->bootstrap = false;
+    
+    parent::__construct();
+
+    $this->displayName = $this->trans('PS Accounts Mock', [], 'Modules.Mymodule.Admin');
+    $this->description = $this->trans('Mocking', [], 'Modules.Mymodule.Admin');
+    $this->confirmUninstall = $this->trans('Are you sure you want to uninstall?', [], 'Modules.Mymodule.Admin');
+
+    require_once __DIR__ . '/vendor/autoload.php';
+    
+    $this->serviceContainer = new \PrestaShop\ModuleLibServiceContainer\DependencyInjection\ServiceContainer(
+      (string) $this->name,
+      $this->getLocalPath()
+    );
+  }
+
+  public function install()
+  {
+    if (parent::install() == false)
+    {
+      return false;
+    }
+    return true;
+  }
+
+  public function uninstall()
+  {
+    if (parent::uninstall() == false)
+    {
+      return false;
+    }
+    return true;
+  }
+
+  public function getService($serviceName)
+  {
+    return $this->serviceContainer->getService($serviceName);
+  }
+}
