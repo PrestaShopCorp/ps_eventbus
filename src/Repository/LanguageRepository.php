86fb93041da654cb3c7ab52ef59fbb38163058c5
John.R <jonathan_renard@hotmail.fr>
2024-02-07 10:58:21 +0100

    feat: add debug for language

--- src/Repository/LanguageRepository.php
+++ src/Repository/LanguageRepository.php
@@ -145,4 +145,28 @@ class LanguageRepository
     {
         return \Language::getLanguages();
     }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * 
+     * @return array
+     * 
+     * @throws \PrestaShopDatabaseException
+     */
+    public function getQueryForDebug($offset, $limit)
+    {
+        $query = $this->getBaseQuery();
+
+        $this->addSelectParameters($query);
+
+        $query->limit($limit, $offset);
+
+        $queryStringified = preg_replace('/\s+/', ' ', $query->build());
+            
+        return [
+            'query' => $query->getQuery(),
+            'queryStringified' => $queryStringified
+        ];
+    }
 }
