4b2713628a3e0ff0cd2d861e19dbe28f224a3b27
John.R <jonathan_renard@hotmail.fr>
2024-02-21 16:23:29 +0100

    fix: error with service contrainer

--- src/Helper/ModuleHelper.php
+++ src/Helper/ModuleHelper.php
@@ -82,7 +82,7 @@ class ModuleHelper
         }
 
         /** @var Router $router * */
-        $router = $this->module->getService('router');
+        $router = $this->module->get('router');
 
         if ($moduleName === 'ps_mbo') {
             return substr(Tools::getShopDomainSsl(true) . __PS_BASE_URI__, 0, -1) .
@@ -112,7 +112,7 @@ class ModuleHelper
         }
 
         /** @var Router $router * */
-        $router = $this->module->getService('router');
+        $router = $this->module->get('router');
 
         return substr(Tools::getShopDomainSsl(true) . __PS_BASE_URI__, 0, -1) .
             $router->generate('admin_module_manage_action', [
@@ -133,7 +133,7 @@ class ModuleHelper
         // need to check if module is up to date, if not, return empty string
 
         /** @var Router $router * */
-        $router = $this->module->getService('router');
+        $router = $this->module->get('router');
 
         return substr(Tools::getShopDomainSsl(true) . __PS_BASE_URI__, 0, -1) .
             $router->generate('admin_module_manage_action', [
