7565249e857593dab5cbfe32e7f23cf08b091780
Benoit Houdayer <benoit.houdayer@prestashop.com>
2024-02-29 13:36:57 +0100

    fix date formats

--- src/Decorator/PayloadDecorator.php
+++ src/Decorator/PayloadDecorator.php
@@ -4,6 +4,8 @@ namespace PrestaShop\Module\PsEventbus\Decorator;
 
 use PrestaShop\Module\PsEventbus\Repository\ConfigurationRepository;
 
+// use hardcoded format to avoid problems with interface change in PHP 7.2
+const ISO8601 = 'Y-m-d\TH:i:sO';
 const DATE_FIELDS = [
   'created_at',
   'updated_at',
@@ -43,7 +45,7 @@ class PayloadDecorator
                 $date = &$payloadItem['properties'][$dateField];
                 if (isset($date) && !empty($date) && $date !== '0000-00-00 00:00:00') {
                     $dateTime = new \DateTime($date, new \DateTimeZone($this->timezone));
-                    $date = $dateTime->format(\DateTime::ISO8601);
+                    $date = $dateTime->format(ISO8601);
                 } else {
                     $date = null;
                 }
