f6cbce5c064c7a5c0b7a73366fff08de430bbebc
Benoit Houdayer <benoit.houdayer@prestashop.com>
2024-02-29 17:49:01 +0100

    add some processing on fixtures and data to make them comparable

--- e2e/src/full-sync-data.spec.ts
+++ e2e/src/full-sync-data.spec.ts
@@ -1,7 +1,7 @@
 import {Controller, doFullSync, probe, PsEventbusSyncUpload} from './helpers/mock-probe';
 import {concatMap, from, lastValueFrom, map, toArray, zip} from "rxjs";
 import testConfig from "./helpers/test.config";
-import {sortUploadData} from "./helpers/data-helper";
+import {generatePredictableModuleId, omitProperties, sortUploadData} from "./helpers/data-helper";
 import {dumpData} from "./helpers/log-helper";
 import R from "ramda";
 import * as matchers from 'jest-extended';
@@ -54,10 +54,6 @@ describe('Full Sync Data', () => {
 
           // load fixtures and filter out fields we know won't exactly match
           const fixture: PsEventbusSyncUpload[] = require(`./fixtures/${controller}.json`);
-          const fixtureWithoutDate: PsEventbusSyncUpload[] = fixture.map(it => ({
-            ...it,
-            properties: R.omit(Object.keys(specialFieldAssert), it.properties)
-          }))
 
           // act
           const syncedData: PsEventbusSyncUpload[] = await lastValueFrom(zip(fullSync$, message$).pipe(
@@ -70,11 +66,23 @@ describe('Full Sync Data', () => {
 
           // dump data for easier debugging or updating fixtures
           if (testConfig.dumpFullSyncData) {
-            dumpData(syncedData, controller);
+            await dumpData(syncedData, controller);
           }
 
+          // we need to process fixtures and data returned from ps_eventbus to make them easier to compare
+          let processedData = syncedData;
+          let processedFixture = fixture;
+          if(controller === 'apiModules' ) {
+            processedData = generatePredictableModuleId(processedData);
+            processedFixture = generatePredictableModuleId(processedFixture);
+          }
+          processedData = sortUploadData(processedData);
+          processedFixture = omitProperties(processedFixture, Object.keys(specialFieldAssert));
+          processedFixture = sortUploadData(processedFixture);
+
           // assert
-          expect(sortUploadData(syncedData)).toMatchObject(sortUploadData(fixtureWithoutDate));
+          expect(processedData).toMatchObject(processedFixture);
+
           // assert special field using custom matcher
           for (const data of syncedData) {
             for (const specialFieldName of Object.keys(specialFieldAssert)) {
