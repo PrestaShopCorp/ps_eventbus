2fd429e4ef7490875877515721ae5162c51a124e
John.R <jonathan_renard@hotmail.fr>
2024-03-20 16:21:56 +0100

    fix: update php-scoper config to match with ps-metrics config

--- scoper.inc.php
+++ scoper.inc.php
@@ -4,64 +4,17 @@ declare(strict_types=1);
 
 use Isolated\Symfony\Component\Finder\Finder;
 
-// You can do your own things here, e.g. collecting symbols to expose dynamically
-// or files to exclude.
-// However beware that this file is executed by PHP-Scoper, hence if you are using
-// the PHAR it will be loaded by the PHAR. So it is highly recommended to avoid
-// to auto-load any code here: it can result in a conflict or even corrupt
-// the PHP-Scoper analysis.
-
-// Example of collecting files to include in the scoped build but to not scope
-// leveraging the isolated finder.
-$excludedFiles = array_map(
-    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathName(),
-    iterator_to_array(
-        Finder::create()->files()->in(__DIR__),
-        false,
-    ),
-);
-
 $version = preg_replace('/\-/', '_', $_SERVER['VERSION']);
 $version = preg_replace('/\./', '_', $version);
 
 return [
-    // The prefix configuration. If a non-null value is used, a random prefix
-    // will be generated instead.
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#prefix
     'prefix' => $version,
-
-    // The base output directory for the prefixed files.
-    // This will be overridden by the 'output-dir' command line option if present.
     'output-dir' => null,
-
-    // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
-    // directory. You can however define which files should be scoped by defining a collection of Finders in the
-    // following configuration key.
-    //
-    // This configuration entry is completely ignored when using Box.
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#finders-and-paths
     'finders' => [
-        Finder::create()->files()->in($_SERVER['TMP_FOLDER']),
-            /* ->exclude('vendor/symfony'), */
+        Finder::create()
+            ->files()
+            ->in($_SERVER['TMP_FOLDER']),
     ],
-
-    // List of excluded files, i.e. files for which the content will be left untouched.
-    // Paths are relative to the configuration file unless if they are already absolute
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
-    'exclude-files' => [
-        // 'src/an-excluded-file.php',
-        // ...$excludedFiles,
-    ],
-
-    // When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
-    // original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
-    // support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
-    // heart contents.
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
     'patchers' => [
         static function (string $filePath, string $prefix, string $contents): string {
             if (strpos($filePath, '/vendor/sentry/sentry/lib/Raven/Client.php')) {
@@ -72,26 +25,46 @@ return [
                 );
             }
 
+            if (strpos($filePath, 'vendor/segmentio/analytics-php/lib/Segment/Client.php')) {
+                return str_replace(
+                    '$consumers = array("socket" => "Segment_Consumer_Socket", "file" => "Segment_Consumer_File", "fork_curl" => "Segment_Consumer_ForkCurl");',
+                    "\$consumers = array(\"socket\" => \"{$prefix}\\Segment_Consumer_Socket\", \"file\" => \"Segment_Consumer_File\", \"fork_curl\" => \"Segment_Consumer_ForkCurl\");",
+                    $contents
+                );
+            }
+
+            if (strpos($filePath, '/ps_eventbus.php')) {
+                return str_replace(
+                    "namespace {$prefix};",
+                    '',
+                    $contents
+                );
+            }
+
+            if (strpos($filePath, '/upgrade') && strpos($filePath, '/vendor') === false) {
+                return str_replace(
+                    "namespace {$prefix};",
+                    '',
+                    $contents
+                );
+            }
+
             return $contents;
         },
     ],
-
-    // List of symbols to consider internal i.e. to leave untouched.
-    //
-    // For more information see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#excluded-symbols
+    'exclude-files' => [],
     'exclude-namespaces' => [
+        '~^PrestaShop\\\\Module\\\\PsAccounts~',
         '~^PrestaShop\\\\Module\\\\PsEventbus~',
         '~^PrestaShop\\\\PrestaShop~',
+        '~^Http\\\\Client~',
         '~^PrestaShopBundle~',
         '~^Symfony~',
-        '~^PrestaShop\\\\Module\\\\PsAccounts~',
-        // 'Acme\Foo'                     // The Acme\Foo namespace (and sub-namespaces)
-        // '~^PHPUnit\\\\Framework$~',    // The whole namespace PHPUnit\Framework (but not sub-namespaces)
-        // '~^$~',                        // The root namespace only
-        // '',                            // Any namespace
+        '~^GuzzleHttp~',
+        '~^Psr~',
     ],
     'exclude-classes' => [
-        // 'ReflectionClassConstant',
+        '\Ps_accounts',
         '\Cache',
         '\Configuration',
         '\Context',
@@ -113,25 +86,12 @@ return [
         '\DateTime',
         '\DateTimeZone',
     ],
-    'exclude-functions' => [
-        // 'mb_str_split',
-    ],
-    'exclude-constants' => [
-        // 'STDIN',
-    ],
-
-    // List of symbols to expose.
-    //
-    // For more information see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#exposed-symbols
+    'exclude-functions' => [],
+    'exclude-constants' => [],
     'expose-global-constants' => true,
-    'expose-global-classes' => true,
+    'expose-global-classes' => false,
     'expose-global-functions' => true,
-    'expose-namespaces' => [
-        // 'Acme\Foo'                     // The Acme\Foo namespace (and sub-namespaces)
-        // '~^PHPUnit\\\\Framework$~',    // The whole namespace PHPUnit\Framework (but not sub-namespaces)
-        // '~^$~',                        // The root namespace only
-        // '',                            // Any namespace
-    ],
+    'expose-namespaces' => [],
     'expose-classes' => [],
     'expose-functions' => [],
     'expose-constants' => [],
