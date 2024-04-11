8b71d5015633da813c362e2df762c983b94bee7b
John.R <jonathan_renard@hotmail.fr>
2024-04-11 16:20:53 +0200

    feat: add 2 script for clean incremental_sync db

---
+++ tools/clean_incremental_sync.sql
@@ -0,0 +1,27 @@
+-- Définir le nom de la base de données
+SET @db_name = 'prestashop';
+set @quantity_needed = '100000';
+
+-- Récupérer le nom de la table avec le prefix
+SET @table_with_prefix = (SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = @db_name
+AND table_name LIKE '%_eventbus_incremental_sync');
+
+-- Supprime les entrées avec plus de 100 000 entrée de ce type
+SET @delete_query = CONCAT('
+	DELETE FROM ', @table_with_prefix, '
+	WHERE type IN (
+		SELECT type
+		FROM (
+			SELECT type, COUNT(*) as incr_type_count
+        	FROM ', @table_with_prefix, '
+        	GROUP BY type
+        	HAVING COUNT(*) >', @quantity_needed, '
+   	 	) AS subquery
+	);
+');
+
+-- Exécuter la requête dynamique directement
+PREPARE dynamic_query FROM @delete_query;
+EXECUTE dynamic_query;
---
+++ tools/count_incremental_sync.sql
@@ -0,0 +1,22 @@
+-- Définir le nom de la base de données et la quantité souhaité pour le clean
+SET @db_name = 'prestashop';
+set @quantity_needed = '100000';
+
+-- Récupérer le nom de la table avec le prefix
+SET @table_with_prefix = (SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = @db_name
+AND table_name LIKE '%_eventbus_incremental_sync');
+
+-- Récupère la quantité d'entrées et le type associé
+SET @get_content_with_extra_counts = CONCAT('
+	SELECT type, 
+	COUNT(*) as incr_type_count
+	FROM ', @table_with_prefix, '
+	GROUP BY type
+	HAVING COUNT(*) >', @quantity_needed, '
+');
+
+-- Exécuter la requête dynamique directement
+PREPARE dynamic_query FROM @get_content_with_extra_counts;
+EXECUTE dynamic_query;
