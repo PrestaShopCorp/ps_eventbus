79a8360dcae1108e26005796233ef5a369926124
John.R <jonathan_renard@hotmail.fr>
2024-04-11 17:56:41 +0200

    feat: add enable full sync query

--- tools/clean_incremental_sync.sql
+++ tools/clean_incremental_sync.sql
@@ -1,27 +1,52 @@
--- Définir le nom de la base de données
+-- Define the name of the database and the quantity desired for the clean
 SET @db_name = 'prestashop';
 set @quantity_needed = '100000';
 
--- Récupérer le nom de la table avec le prefix
-SET @table_with_prefix = (SELECT table_name
+-- Retrieve the _eventbus_type_sync table name with prefix
+SET @eventbus_type_sync_table = (SELECT table_name
 FROM information_schema.tables
 WHERE table_schema = @db_name
-AND table_name LIKE '%_eventbus_incremental_sync');
+AND table_name LIKE '%_eventbus_type_sync');
 
--- Supprime les entrées avec plus de X entrée de ce type (paramétré au dessus via la variable @quantity_needed)
-SET @delete_query = CONCAT('
-	DELETE FROM ', @table_with_prefix, '
+-- enable full-sync for selected shop content // TODO DEBUG
+SET @enable_full_sync = CONCAT('
+	UPDATE ', @eventbus_type_sync_table, '
+	SET `offset` = 1
 	WHERE type IN (
 		SELECT type
 		FROM (
 			SELECT type, COUNT(*) as incr_type_count
-        	FROM ', @table_with_prefix, '
+        	FROM ', @eventbus_incremental_sync_table, '
         	GROUP BY type
         	HAVING COUNT(*) > ', @quantity_needed, '
    	 	) AS subquery
 	);
 ');
 
--- Exécuter la requête dynamique directement
-PREPARE dynamic_query FROM @delete_query;
-EXECUTE dynamic_query;
+-- Execute dynamic query
+PREPARE enable_full_sync FROM @enable_full_sync;
+EXECUTE enable_full_sync;
+
+-- Retrieve the eventbus_incremental_sync table name with prefix
+SET @eventbus_incremental_sync_table = (SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = @db_name
+AND table_name LIKE '%_eventbus_incremental_sync');
+
+-- Delete entries with more than X entries of this type (set above via the variable @quantity_needed)
+SET @delete_query = CONCAT('
+	DELETE FROM ', @eventbus_incremental_sync_table, '
+	WHERE type IN (
+		SELECT type
+		FROM (
+			SELECT type, COUNT(*) as incr_type_count
+        	FROM ', @eventbus_incremental_sync_table, '
+        	GROUP BY type
+        	HAVING COUNT(*) > ', @quantity_needed, '
+   	 	) AS subquery
+	);
+');
+
+-- Execute dynamic query
+PREPARE delete_query FROM @delete_query;
+EXECUTE delete_query;
--- tools/count_incremental_sync.sql
+++ tools/count_incremental_sync.sql
@@ -1,22 +1,22 @@
--- Définir le nom de la base de données et la quantité souhaité pour le clean
+-- Define the name of the database and the quantity desired for the clean
 SET @db_name = 'prestashop';
 set @quantity_needed = '100000';
 
--- Récupérer le nom de la table avec le prefix
-SET @table_with_prefix = (SELECT table_name
+-- Retrieve the eventbus_incremental_sync table name with prefix
+SET @eventbus_incremental_sync_table = (SELECT table_name
 FROM information_schema.tables
 WHERE table_schema = @db_name
 AND table_name LIKE '%_eventbus_incremental_sync');
 
--- Récupère la quantité d'entrées et le type associé
+-- Retrieves the input quantity and associated type
 SET @get_content_with_extra_counts = CONCAT('
 	SELECT type, 
 	COUNT(*) as incr_type_count
-	FROM ', @table_with_prefix, '
+	FROM ', @eventbus_incremental_sync_table, '
 	GROUP BY type
 	HAVING COUNT(*) > ', @quantity_needed, '
 ');
 
--- Exécuter la requête dynamique directement
-PREPARE dynamic_query FROM @get_content_with_extra_counts;
-EXECUTE dynamic_query;
+-- Execute dynamic query
+PREPARE get_content_with_extra_counts FROM @get_content_with_extra_counts;
+EXECUTE get_content_with_extra_counts;
