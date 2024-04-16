cabf2b61a99e94d3107847ae49e05db65a4f958e
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2024-04-16 14:43:50 +0200

    CI fix

--- e2e/src/full-sync.spec.ts
+++ e2e/src/full-sync.spec.ts
@@ -1,158 +1,179 @@
-import testConfig from './helpers/test.config';
-import * as matchers from 'jest-extended';
-import {dumpUploadData, logAxiosError} from "./helpers/log-helper";
-import axios, {AxiosError} from "axios";
-import {doFullSync, probe, PsEventbusSyncUpload} from "./helpers/mock-probe";
-import {concatMap, from, lastValueFrom, map, toArray, zip} from "rxjs";
+import testConfig from "./helpers/test.config";
+import * as matchers from "jest-extended";
+import { dumpUploadData, logAxiosError } from "./helpers/log-helper";
+import axios, { AxiosError } from "axios";
+import { doFullSync, probe, PsEventbusSyncUpload } from "./helpers/mock-probe";
+import { concatMap, from, lastValueFrom, map, toArray, zip } from "rxjs";
 import {
   generatePredictableModuleId,
   loadFixture,
   omitProperties,
-  sortUploadData
+  sortUploadData,
 } from "./helpers/data-helper";
-import {Controller, controllerList} from "./helpers/controllers";
+import { Controller, controllerList } from "./helpers/controllers";
 
 expect.extend(matchers);
 
 // these controllers will be excluded from the following test suite
-const EXCLUDED_API: Controller[] = ['apiGoogleTaxonomies'];
+const EXCLUDED_API: Controller[] = ["apiGoogleTaxonomies"];
 
 // FIXME : these api can't send anything to the mock api because the database is empty from the factory
-const MISSING_TEST_DATA: Controller[] = ['apiCartRules', 'apiCustomProductCarriers', 'apiTranslations', 'apiWishlists'];
+const MISSING_TEST_DATA: Controller[] = [
+  "apiCartRules",
+  "apiCustomProductCarriers",
+  "apiTranslations",
+  "apiWishlists",
+];
 
 // these fields change from test run to test run, so we replace them with a matcher to only ensure the type and format are correct
-const isDateString = val => val ? expect(val).toBeDateString() : expect(val).toBeNull();
-const isString = val => val ? expect(val).toBeString() : expect(val).toBeNull();
-const isNumber = val => val ? expect(val).toBeNumber() : expect(val).toBeNull();
+const isDateString = (val) =>
+  val ? expect(val).toBeDateString() : expect(val).toBeNull();
+const isString = (val) =>
+  val ? expect(val).toBeString() : expect(val).toBeNull();
+const isNumber = (val) =>
+  val ? expect(val).toBeNumber() : expect(val).toBeNull();
 const specialFieldAssert: { [index: string]: (val) => void } = {
-  'created_at': isDateString,
-  'updated_at': isDateString,
-  'last_connection_date': isDateString,
-  'folder_created_at': isDateString,
-  'date_add': isDateString,
-  'from': isDateString,
-  'to': isDateString,
-  'conversion_rate': isNumber,
-  'cms_version': isString,
-  'module_id': isString,
-  'module_version': isString,
-  'theme_version': isString,
-}
+  created_at: isDateString,
+  updated_at: isDateString,
+  last_connection_date: isDateString,
+  folder_created_at: isDateString,
+  date_add: isDateString,
+  from: isDateString,
+  to: isDateString,
+  conversion_rate: isNumber,
+  cms_version: isString,
+  module_id: isString,
+  module_version: isString,
+  theme_version: isString,
+  php_version: isString,
+  http_server: isString,
+};
 
-describe('Full Sync', () => {
+describe("Full Sync", () => {
   let testIndex = 0;
 
-  const controllers: Controller[] = controllerList.filter(it => !EXCLUDED_API.includes(it))
+  const controllers: Controller[] = controllerList.filter(
+    (it) => !EXCLUDED_API.includes(it)
+  );
 
   let jobId: string;
 
   beforeEach(() => {
-    jobId = `valid-job-full-${testIndex++}`
+    jobId = `valid-job-full-${testIndex++}`;
   });
 
   // TODO : some versions of prestashop include ps_facebook out of the box, this test can't reliably be run for all versions
-  describe.skip('apiGoogleTaxonomies', () => {
-    const controller = 'apiGoogleTaxonomies'
+  describe.skip("apiGoogleTaxonomies", () => {
+    const controller = "apiGoogleTaxonomies";
 
     // TODO : apiGoogleTaxonomies requires an additional module to be present : devise a specific test setup for this endpoint
-    it.skip(`${controller} should accept full sync`, async () => {
-    });
+    it.skip(`${controller} should accept full sync`, async () => {});
 
-    it.skip(`${controller} should upload to collector`, async () => {
-    });
+    it.skip(`${controller} should upload to collector`, async () => {});
 
     it(`${controller} should reject full sync when ps_facebook is not installed`, async () => {
       // arrange
       const url = `${testConfig.prestashopUrl}/index.php?fc=module&module=ps_eventbus&controller=${controller}&limit=5&full=1&job_id=${jobId}`;
 
       // act
-      const response = await axios.post(url, {
-        headers: {'Host': testConfig.prestaShopHostHeader},
-      }).catch(err => {
-        expect(err).toBeInstanceOf(AxiosError);
-        return err.response
-      });
+      const response = await axios
+        .post(url, {
+          headers: { Host: testConfig.prestaShopHostHeader },
+        })
+        .catch((err) => {
+          expect(err).toBeInstanceOf(AxiosError);
+          return err.response;
+        });
 
       // assert
       expect(response.status).toEqual(456);
       // expect some explanation to be given to the user
-      expect(response.statusText).toMatch(/[Ff]acebook/)
-    })
-  })
+      expect(response.statusText).toMatch(/[Ff]acebook/);
+    });
+  });
 
-  describe.each(controllers)('%s', (controller) => {
-
+  describe.each(controllers)("%s", (controller) => {
     it(`${controller} should accept full sync`, async () => {
       // arrange
       const url = `${testConfig.prestashopUrl}/index.php?fc=module&module=ps_eventbus&controller=${controller}&limit=5&full=1&job_id=${jobId}`;
 
       // act
-      const response = await axios.post(url, {
-        headers: {
-          'Host': testConfig.prestaShopHostHeader
-        },
-      }).catch(err => {
-        logAxiosError(err);
-        expect(err).toBeNull();
-        throw err;
-      });
+      const response = await axios
+        .post(url, {
+          headers: {
+            Host: testConfig.prestaShopHostHeader,
+          },
+        })
+        .catch((err) => {
+          logAxiosError(err);
+          expect(err).toBeNull();
+          throw err;
+        });
 
       // assert
-      expect(response.status).toBeOneOf([200, 201])
-      expect(response.headers).toMatchObject({'content-type': /json/})
+      expect(response.status).toBeOneOf([200, 201]);
+      expect(response.headers).toMatchObject({ "content-type": /json/ });
       expect(response.data).toMatchObject({
-        'job_id': jobId,
-        'syncType': 'full'
-      })
+        job_id: jobId,
+        syncType: "full",
+      });
     });
 
     if (MISSING_TEST_DATA.includes(controller)) {
-      it.skip(`${controller} should upload to collector`, () => {})
+      it.skip(`${controller} should upload to collector`, () => {});
     } else {
       it(`${controller} should upload to collector`, async () => {
         // arrange
         const url = `${testConfig.prestashopUrl}/index.php?fc=module&module=ps_eventbus&controller=${controller}&limit=5&full=1&job_id=${jobId}`;
-        const message$ = probe({url: `/upload/${jobId}`});
+        const message$ = probe({ url: `/upload/${jobId}` });
 
         // act
-        const request$ = from(axios.post(url, {
-          headers: {
-            'Host': testConfig.prestaShopHostHeader
-          },
-        }));
+        const request$ = from(
+          axios.post(url, {
+            headers: {
+              Host: testConfig.prestaShopHostHeader,
+            },
+          })
+        );
 
-        const results = await lastValueFrom(zip(message$, request$)
-          .pipe(map(result => ({
+        const results = await lastValueFrom(
+          zip(message$, request$).pipe(
+            map((result) => ({
               probeMessage: result[0],
               psEventbusReq: result[1],
             })),
-            toArray()));
+            toArray()
+          )
+        );
 
         // assert
         expect(results.length).toEqual(1);
-        expect(results[0].probeMessage.method).toBe('POST');
-        expect(results[0].probeMessage.headers).toMatchObject({"full-sync-requested": "1"});
+        expect(results[0].probeMessage.method).toBe("POST");
+        expect(results[0].probeMessage.headers).toMatchObject({
+          "full-sync-requested": "1",
+        });
       });
     }
 
     if (MISSING_TEST_DATA.includes(controller)) {
-      it.skip(`${controller} should upload complete dataset to collector`, () => {
-      })
+      it.skip(`${controller} should upload complete dataset to collector`, () => {});
     } else {
       it(`${controller} should upload complete dataset collector`, async () => {
         // arrange
-        const fullSync$ = doFullSync(jobId, controller, {timeout: 4000});
-        const message$ = probe({url: `/upload/${jobId}`}, {timeout: 4000})
+        const fullSync$ = doFullSync(jobId, controller, { timeout: 4000 });
+        const message$ = probe({ url: `/upload/${jobId}` }, { timeout: 4000 });
         const fixture = await loadFixture(controller);
 
         // act
-        const syncedData: PsEventbusSyncUpload[] = await lastValueFrom(zip(fullSync$, message$).pipe(
-          map(msg => msg[1].body.file),
-          concatMap(syncedPage => {
-            return from(syncedPage);
-          }),
-          toArray(),
-        ))
+        const syncedData: PsEventbusSyncUpload[] = await lastValueFrom(
+          zip(fullSync$, message$).pipe(
+            map((msg) => msg[1].body.file),
+            concatMap((syncedPage) => {
+              return from(syncedPage);
+            }),
+            toArray()
+          )
+        );
 
         // dump data for easier debugging or updating fixtures
         if (testConfig.dumpFullSyncData) {
@@ -162,13 +183,19 @@ describe('Full Sync', () => {
         // we need to process fixtures and data returned from ps_eventbus to make them easier to compare
         let processedData = syncedData;
         let processedFixture = fixture;
-        if (controller === 'apiModules') {
+        if (controller === "apiModules") {
           processedData = generatePredictableModuleId(processedData);
           processedFixture = generatePredictableModuleId(processedFixture);
         }
-        processedData = omitProperties(processedData, Object.keys(specialFieldAssert));
+        processedData = omitProperties(
+          processedData,
+          Object.keys(specialFieldAssert)
+        );
         processedData = sortUploadData(processedData);
-        processedFixture = omitProperties(processedFixture, Object.keys(specialFieldAssert));
+        processedFixture = omitProperties(
+          processedFixture,
+          Object.keys(specialFieldAssert)
+        );
         processedFixture = sortUploadData(processedFixture);
 
         // assert
@@ -178,13 +205,13 @@ describe('Full Sync', () => {
         for (const data of syncedData) {
           for (const specialFieldName of Object.keys(specialFieldAssert)) {
             if (data.properties[specialFieldName] !== undefined) {
-              specialFieldAssert[specialFieldName](data.properties[specialFieldName]);
+              specialFieldAssert[specialFieldName](
+                data.properties[specialFieldName]
+              );
             }
           }
         }
       });
     }
-
-  })
-})
-
+  });
+});
