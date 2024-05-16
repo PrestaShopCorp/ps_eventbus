03326dc8cbdb1bd8cbac48f46e335dfb7ad88ee1
John.R <jonathan_renard@hotmail.fr>
2024-05-16 16:13:33 +0200

    fix: name of tables

--- upgrade/Upgrade-3.0.12.php
+++ upgrade/Upgrade-3.0.12.php
@@ -13,11 +13,14 @@ function upgrade_module_3_0_12($module)
 
     // If previous installed version number is between 3.0.8 and 3.0.11, truncate _eventbus_incremental_sync and _eventbus_type_sync tables
     if (in_array($result[0]['version'], ['3.0.8', '3.0.9', '3.0.10', '3.0.11'])) {
-        $truncateIncrementalSyncTable = 'TRUNCATE TABLE `' . _DB_PREFIX_ . '_eventbus_incremental_sync`;';
-        $truncateTypeSyncTable = 'TRUNCATE TABLE `' . _DB_PREFIX_ . '_eventbus_type_sync`;';
+        $truncateIncrementalSyncTable = 'TRUNCATE TABLE `' . _DB_PREFIX_ . 'eventbus_incremental_sync`;';
+        $truncateTypeSyncTable = 'TRUNCATE TABLE `' . _DB_PREFIX_ . 'eventbus_type_sync`;';
 
         $db->execute($truncateIncrementalSyncTable);
         $db->execute($truncateTypeSyncTable);
+
+        dump('db truncated');
+        exit();
     }
 
     return true;
