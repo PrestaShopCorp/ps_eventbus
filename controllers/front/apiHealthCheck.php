ee6cdd0930e151fbf3b12b62928d8effa552cc32
Jonathan Renard <1273438+fox-john@users.noreply.github.com>
2024-05-23 10:37:53 +0200

    feat: add auth to healthcheck (#294)

--- controllers/front/apiHealthCheck.php
+++ controllers/front/apiHealthCheck.php
@@ -2,32 +2,41 @@
 
 use PrestaShop\Module\PsEventbus\Config\Config;
 use PrestaShop\Module\PsEventbus\Controller\AbstractApiController;
+use PrestaShop\Module\PsEventbus\Exception\UnauthorizedException;
 use PrestaShop\Module\PsEventbus\Repository\ServerInformationRepository;
 
 class ps_EventbusApiHealthCheckModuleFrontController extends AbstractApiController
 {
     public $type = Config::COLLECTION_SHOPS;
 
+    private $isAuthentifiedCall = true;
+
     /**
-     * Override default method from AbstractApiController: skip jobId verification
+     * Override default method from AbstractApiController
+     * Get another behavior for healthcheck
      *
      * @return void
      */
     public function init()
     {
+        try {
+            parent::init();
+        } catch (UnauthorizedException $exception) {
+            $this->isAuthentifiedCall = false;
+        }
     }
 
     /**
      * @return void
      *
-     * @throws\PrestaShopException
+     * @throws \PrestaShopException
      */
     public function postProcess()
     {
         /** @var ServerInformationRepository $serverInformationRepository */
         $serverInformationRepository = $this->module->getService(ServerInformationRepository::class);
 
-        $status = $serverInformationRepository->getHealthCheckData();
+        $status = $serverInformationRepository->getHealthCheckData($this->isAuthentifiedCall);
 
         $this->exitWithResponse($status);
     }
