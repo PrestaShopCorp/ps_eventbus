78cf523ee5860573efb26f7606970f1b8a8f45e2
John.R <jonathan_renard@hotmail.fr>
2024-06-07 15:55:57 +0200

    fix: typo

--- upgrade/Upgrade-3.1.0.php
+++ upgrade/Upgrade-3.1.0.php
@@ -9,9 +9,9 @@ function upgrade_module_3_1_0()
 {
     $db = Db::getInstance();
 
-    $db->update('eventbus_deleted_object', ['type' => Config::COLLECTION_CUSTOM_PRODUCT_CARRIERS], '`type` = "custom_product_carrier"');
-    $db->update('eventbus_deleted_object', ['type' => Config::COLLECTION_SPECIFIC_PRICES], '`type` = "specific_price"');
-    $db->update('eventbus_deleted_object', ['type' => Config::COLLECTION_CARRIERS], '`type` = "carrier"');
+    $db->update('eventbus_deleted_objects', ['type' => Config::COLLECTION_CUSTOM_PRODUCT_CARRIERS], '`type` = "custom_product_carrier"');
+    $db->update('eventbus_deleted_objects', ['type' => Config::COLLECTION_SPECIFIC_PRICES], '`type` = "specific_price"');
+    $db->update('eventbus_deleted_objects', ['type' => Config::COLLECTION_CARRIERS], '`type` = "carrier"');
 
     return true;
 }
