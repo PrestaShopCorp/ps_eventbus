8791a762ad27233a0b78a5c3cb39055a18323991
John.R <jonathan_renard@hotmail.fr>
2024-07-01 15:35:05 +0200

    fix: php-cs-fixer

--- controllers/front/apiHealthCheck.php
+++ controllers/front/apiHealthCheck.php
@@ -20,12 +20,12 @@ class ps_EventbusApiHealthCheckModuleFrontController extends AbstractApiControll
      */
     public function init()
     {
-        /* 
+        /*
         try {
             parent::init();
         } catch (UnauthorizedException $exception) {
             $this->isAuthentifiedCall = false;
-        } 
+        }
         */
     }
 
