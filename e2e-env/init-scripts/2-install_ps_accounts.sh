ba3b52ebb3635c834fea12193434394fa50aff98
dparis <david.paris@prestashop.com>
2024-07-05 15:00:09 +0200

    actions/workflows e2e matrix: prestashop & php versions

--- e2e-env/init-scripts/2-install_ps_accounts.sh
+++ e2e-env/init-scripts/2-install_ps_accounts.sh
@@ -3,8 +3,8 @@ set -eu
 cd "$(dirname $0)" || exit 1
 
 echo "* [ps_accounts_mock] downloading..."
-echo "https://github.com/PrestaShopCorp/ps_accounts_mock/releases/download/${PS_ACCOUNTS_MOCK_VERSION}/ps_accounts_mock-${PS_ACCOUNTS_MOCK_VERSION:1}.zip"
-wget -q -O /tmp/ps_accounts.zip "https://github.com/PrestaShopCorp/ps_accounts_mock/releases/download/${PS_ACCOUNTS_MOCK_VERSION}/ps_accounts_mock-${PS_ACCOUNTS_MOCK_VERSION:1}.zip"
+echo "https://github.com/PrestaShopCorp/ps_accounts_mock/releases/download/${PS_ACCOUNTS_MOCK_VERSION}/ps_accounts_mock-${PS_ACCOUNTS_MOCK_VERSION}.zip"
+wget -q -O /tmp/ps_accounts.zip "https://github.com/PrestaShopCorp/ps_accounts_mock/releases/download/${PS_ACCOUNTS_MOCK_VERSION}/ps_accounts_mock-${PS_ACCOUNTS_MOCK_VERSION}.zip"
 echo "* [ps_accounts_mock] unziping..."
 unzip -qq /tmp/ps_accounts.zip -d /var/www/html/modules
 echo "* [ps_accounts_mock] installing the module..."
