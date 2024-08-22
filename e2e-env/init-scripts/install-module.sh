5deff791ef0fdfd3af0898da81e54374a0e167af
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2024-08-22 17:30:35 +0200

    Fix E2E env with flashlight www-data (#343)

--- e2e-env/init-scripts/install-module.sh
+++ e2e-env/init-scripts/install-module.sh
@@ -12,43 +12,24 @@ error() {
   exit "${2:-1}"
 }
 
-run_user() {
-  sudo -g www-data -u www-data -- "$@"
-}
-
 ps_accounts_mock_install() {
   echo "* [ps_accounts_mock] downloading..."
   PS_ACCOUNTS_MOCK_VERSION="v7.0.2"
   echo "* [ps_accounts_mock] downloading..."
   wget -q -O /tmp/ps_accounts.zip "https://github.com/PrestaShopCorp/ps_accounts_mock/releases/download/${PS_ACCOUNTS_MOCK_VERSION}/ps_accounts_mock-${PS_ACCOUNTS_MOCK_VERSION}.zip"
   echo "* [ps_accounts_mock] unziping..."
-  run_user unzip -qq /tmp/ps_accounts.zip -d /var/www/html/modules
+  unzip -qq /tmp/ps_accounts.zip -d /var/www/html/modules
   echo "* [ps_accounts_mock] installing the module..."
   cd "$PS_FOLDER"
-  run_user php -d memory_limit=-1 bin/console prestashop:module --no-interaction install "ps_accounts"
+  php -d memory_limit=-1 bin/console prestashop:module --no-interaction install "ps_accounts"
 }
 
 ps_eventbus_install() {
-  # Some explanations are required here:
-  #
-  # If you look closer to the ./docker-compose.yml prestashop service, you will
-  # see multiple mounts on the same files:
-  # - ..:/var/www/html/modules/ps_eventbus:rw        => mount all the sources
-  # - /var/www/html/modules/ps_eventbus/vendor       => void the specific vendor dir, makint it empty
-  # - /var/www/html/modules/ps_eventbus/tools/vendor => void the specific vendor dev dir, making it empty
-  #
-  # That said, we now want our container to have RW access on these directories,
-  # and to install the required composer dependencies for the module to work.
-  #
-  # Other scenarios could be imagined, but this is the best way to avoid writes on a mounted volume,
-  # which would not work on a Linux environment (binding a volume), as opposed to a Windows or Mac one (NFS mount).
-  chown www-data:www-data ./modules/ps_eventbus/vendor
-  chown www-data:www-data ./modules/ps_eventbus/tools/vendor
-  run_user composer install -n -d ./modules/ps_eventbus
-
+  # Notice: you might enable this if your uid is not 1000, or encounter permission issues
+  # composer install -n -d ./modules/ps_eventbus
   echo "* [ps_eventbus] installing the module..."
   cd "$PS_FOLDER"
-  run_user php -d memory_limit=-1 bin/console prestashop:module --no-interaction install "ps_eventbus"
+  php -d memory_limit=-1 bin/console prestashop:module --no-interaction install "ps_eventbus"
 }
 
 ps_accounts_mock_install
