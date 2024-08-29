2d0c2d0f0b93e9aa6fd23fe8fbdae358be0fafa2
John.R <jonathan_renard@hotmail.fr>
2024-08-29 14:31:33 +0200

    fix: throwed error management

--- src/Service/FrontApiService.php
+++ src/Service/FrontApiService.php
@@ -2,6 +2,8 @@
 
 namespace PrestaShop\Module\PsEventbus\Service;
 
+use LogicException;
+use Exception;
 use PrestaShop\Module\PsEventbus\Config\Config;
 use PrestaShop\Module\PsEventbus\Exception\EnvVarException;
 use PrestaShop\Module\PsEventbus\Exception\FirebaseException;
@@ -11,7 +13,11 @@ use PrestaShop\Module\PsEventbus\Repository\ConfigurationRepository;
 use PrestaShop\Module\PsEventbus\Repository\EventbusSyncRepository;
 use PrestaShop\Module\PsEventbus\Repository\IncrementalSyncRepository;
 use PrestaShop\Module\PsEventbus\Repository\LanguageRepository;
+use PrestaShopException;
+use PrestaShopDatabaseException;
+use Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException;
 use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+use Symfony\Component\Translation\Exception\InvalidArgumentException;
 
 class FrontApiService
 {
@@ -178,8 +184,23 @@ class FrontApiService
         } catch (FirebaseException $exception) {
             $this->errorHandler->handle($exception);
             CommonService::exitWithExceptionMessage($exception);
-        } catch (\Exception|ServiceNotFoundException $exception) {
-            $this->errorHandler->handle($exception);
+        } catch (ServiceNotFoundException $exception) {
+            $this->catchGenericException($exception, $ise2e);
+        } catch (\Exception $exception) {
+            $this->catchGenericException($exception, $ise2e);
+        }
+    }
+
+    /**
+     * 
+     * @param mixed $exception 
+     * @param mixed $ise2e 
+     * @return void
+     * 
+     * @throws Exception 
+     */
+    private function catchGenericException($exception, $ise2e) {
+        $this->errorHandler->handle($exception);
 
             // if debug mode enabled, print error
             if (_PS_MODE_DEV_ == true && $ise2e == false) {
@@ -187,7 +208,6 @@ class FrontApiService
             }
 
             CommonService::dieWithResponse(['message' => 'An error occured. Please check shop logs for more information'], 500);
-        }
     }
 
     /**
