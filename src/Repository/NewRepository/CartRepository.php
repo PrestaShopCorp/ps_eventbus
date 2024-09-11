d31ac6c7610e1ff07070e18006e13a543c028ffb
John.R <jonathan_renard@hotmail.fr>
2024-09-11 14:36:05 +0200

    feat: add carts

---
+++ src/Repository/NewRepository/CartRepository.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace PrestaShop\Module\PsEventbus\Repository\NewRepository;
+
+class CartRepository extends AbstractRepository implements RepositoryInterface
+{
+    const TABLE_NAME = 'cart';
+
+        /**
+     * @return void
+     */
+    public function generateMinimalQuery()
+    {
+        $this->query = new \DbQuery();
+
+        $this->query->from(self::TABLE_NAME, 'c');
+    }
+
+    /**
+     * @param string $langIso
+     *
+     * @return mixed
+     *
+     * @throws \PrestaShopException
+     */
+    public function generateFullQuery($langIso)
+    {
+        $this->generateMinimalQuery();
+
+        $this->query->where('c.id_shop = ' . (int) parent::getShopId());
+
+        $this->query
+            ->select('c.id_cart')
+            ->select('date_add as created_at')
+            ->select('date_upd as updated_at')
+        ;
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     *
+     * @throws \PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     */
+    public function getContentsForFull($offset, $limit, $langIso, $debug)
+    {
+        $this->generateFullQuery($langIso);
+
+        $this->query->limit((int) $limit, (int) $offset);
+
+        return $this->runQuery($debug);
+    }
+
+    /**
+     * @param int $limit
+     * @param array<mixed> $contentIds
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     *
+     * @throws \PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     */
+    public function getContentsForIncremental($limit, $contentIds, $langIso, $debug)
+    {
+        $this->generateFullQuery($langIso);
+
+        $this->query
+            ->where('c.id_cart IN(' . implode(',', array_map('intval', $contentIds)) . ')')
+            ->limit($limit)
+        ;
+
+        return $this->runQuery($debug);
+    }
+
+    /**
+     * @param int $offset
+     *
+     * @return int
+     *
+     * @throws \PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     */
+    public function countFullSyncContentLeft($offset)
+    {
+        $this->generateMinimalQuery();
+
+        $this->query->select('(COUNT(c.id_cart) - ' . (int) $offset . ') as count');
+
+        $result = $this->runQuery(false);
+
+        return $result[0]['count'];
+    }
+}
