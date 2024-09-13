a11a6ee4f9bd44bc9fa2418ccee9cd85c4ba2df8
Jonathan Renard <1273438+fox-john@users.noreply.github.com>
2024-09-13 10:18:04 +0200

    feat: pseventbus v4 cart rules (#358)

---
+++ src/Repository/NewRepository/CartRuleRepository.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace PrestaShop\Module\PsEventbus\Repository\NewRepository;
+
+class CartRuleRepository extends AbstractRepository implements RepositoryInterface
+{
+    const TABLE_NAME = 'cart_rule';
+
+    /**
+     * @return void
+     */
+    public function generateMinimalQuery()
+    {
+        $this->query = new \DbQuery();
+
+        $this->query->from(self::TABLE_NAME, 'cr');
+    }
+
+    /**
+     * @param string $langIso
+     *
+     * @return mixed
+     *
+     * @throws \PrestaShopException
+     */
+    public function generateFullQuery($langIso)
+    {
+        $this->generateMinimalQuery();
+
+        $this->query
+            ->select('cr.id_cart_rule')
+            ->select('cr.id_customer')
+            ->select('cr.code')
+            ->select('cr.date_from AS "from"')
+            ->select('cr.date_to AS "to"')
+            ->select('cr.description')
+            ->select('cr.quantity')
+            ->select('cr.quantity_per_user')
+            ->select('cr.priority')
+            ->select('cr.partial_use')
+            ->select('cr.minimum_amount')
+            ->select('cr.minimum_amount_tax')
+            ->select('cr.minimum_amount_currency')
+            ->select('cr.minimum_amount_shipping')
+            ->select('cr.country_restriction')
+            ->select('cr.carrier_restriction')
+            ->select('cr.group_restriction')
+            ->select('cr.cart_rule_restriction')
+            ->select('cr.product_restriction')
+            ->select('cr.shop_restriction')
+            ->select('cr.free_shipping')
+            ->select('cr.reduction_percent')
+            ->select('cr.reduction_amount')
+            ->select('cr.reduction_tax')
+            ->select('cr.reduction_currency')
+            ->select('cr.reduction_product')
+            ->select('cr.gift_product')
+            ->select('cr.gift_product_attribute')
+            ->select('cr.highlight')
+            ->select('cr.active')
+            ->select('cr.date_add AS created_at')
+            ->select('cr.date_upd AS updated_at')
+        ;
+
+        if (defined('_PS_VERSION_') && version_compare(_PS_VERSION_, '1.7', '>=')) {
+            $this->query->select('cr.reduction_exclude_special');
+        }
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     *
+     * @throws \PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     */
+    public function getContentsForFull($offset, $limit, $langIso, $debug)
+    {
+        $this->generateFullQuery($langIso);
+
+        $this->query->limit((int) $limit, (int) $offset);
+
+        return $this->runQuery($debug);
+    }
+
+    /**
+     * @param int $limit
+     * @param array<mixed> $contentIds
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     *
+     * @throws \PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     */
+    public function getContentsForIncremental($limit, $contentIds, $langIso, $debug)
+    {
+        $this->generateFullQuery($langIso);
+
+        $this->query
+            ->where('cr.id_cart_rule IN(' . implode(',', array_map('intval', $contentIds)) . ')')
+            ->limit($limit)
+        ;
+
+        return $this->runQuery($debug);
+    }
+
+    /**
+     * @param int $offset
+     *
+     * @return int
+     *
+     * @throws \PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     */
+    public function countFullSyncContentLeft($offset)
+    {
+        $this->generateMinimalQuery();
+
+        $this->query->select('(COUNT(cr.id_cart_rule) - ' . (int) $offset . ') as count');
+
+        $result = $this->runQuery(false);
+
+        return $result[0]['count'];
+    }
+}
---
+++ src/Service/ShopContent/CartRulesService.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace PrestaShop\Module\PsEventbus\Service\ShopContent;
+
+use PrestaShop\Module\PsEventbus\Config\Config;
+use PrestaShop\Module\PsEventbus\Repository\NewRepository\CartRuleRepository;
+
+class CartRulesService implements ShopContentServiceInterface
+{
+    /** @var CartRuleRepository */
+    private $cartRuleRepository;
+
+    public function __construct(CartRuleRepository $cartRuleRepository)
+    {
+        $this->cartRuleRepository = $cartRuleRepository;
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     */
+    public function getContentsForFull($offset, $limit, $langIso, $debug)
+    {
+        $result = $this->cartRuleRepository->getContentsForFull($offset, $limit, $langIso, $debug);
+
+        if (empty($result)) {
+            return [];
+        }
+
+        $this->castCartRules($result);
+
+        return array_map(function ($item) {
+            return [
+                'id' => $item['id_cart_rule'],
+                'collection' => Config::COLLECTION_CART_RULES,
+                'properties' => $item,
+            ];
+        }, $result);
+    }
+
+    /**
+     * @param int $limit
+     * @param array<string, int> $contentIds
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     */
+    public function getContentsForIncremental($limit, $contentIds, $langIso, $debug)
+    {
+        $result = $this->cartRuleRepository->getContentsForIncremental($limit, $contentIds, $langIso, $debug);
+
+        if (empty($result)) {
+            return [];
+        }
+
+        $this->castCartRules($result);
+
+        return array_map(function ($item) {
+            return [
+                'id' => $item['id_cart_rule'],
+                'collection' => Config::COLLECTION_CART_RULES,
+                'properties' => $item,
+            ];
+        }, $result);
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @param string $langIso
+     *
+     * @return int
+     */
+    public function countFullSyncContentLeft($offset, $limit, $langIso)
+    {
+        return $this->cartRuleRepository->countFullSyncContentLeft($offset);
+    }
+
+    /**
+     * @param array<mixed> $cartRules
+     *
+     * @return void
+     */
+    private function castCartRules(&$cartRules)
+    {
+        foreach ($cartRules as &$cartRule) {
+            $cartRule['id_cart_rule'] = (int) $cartRule['id_cart_rule'];
+            $cartRule['id_customer'] = (int) $cartRule['id_customer'];
+            $cartRule['quantity'] = (int) $cartRule['quantity'];
+            $cartRule['quantity_per_user'] = (int) $cartRule['quantity_per_user'];
+            $cartRule['priority'] = (int) $cartRule['priority'];
+            $cartRule['partial_use'] = (bool) $cartRule['partial_use'];
+            $cartRule['minimum_amount'] = (float) $cartRule['minimum_amount'];
+            $cartRule['minimum_amount_tax'] = (bool) $cartRule['minimum_amount_tax'];
+            $cartRule['minimum_amount_currency'] = (int) $cartRule['minimum_amount_currency'];
+            $cartRule['minimum_amount_shipping'] = (bool) $cartRule['minimum_amount_shipping'];
+            $cartRule['country_restriction'] = (bool) $cartRule['country_restriction'];
+            $cartRule['carrier_restriction'] = (bool) $cartRule['carrier_restriction'];
+            $cartRule['group_restriction'] = (bool) $cartRule['group_restriction'];
+            $cartRule['cart_rule_restriction'] = (bool) $cartRule['cart_rule_restriction'];
+            $cartRule['product_restriction'] = (bool) $cartRule['product_restriction'];
+            $cartRule['shop_restriction'] = (bool) $cartRule['shop_restriction'];
+            $cartRule['free_shipping'] = (bool) $cartRule['free_shipping'];
+            $cartRule['reduction_percent'] = (float) $cartRule['reduction_percent'];
+            $cartRule['reduction_amount'] = (float) $cartRule['reduction_amount'];
+            $cartRule['reduction_tax'] = (bool) $cartRule['reduction_tax'];
+            $cartRule['reduction_currency'] = (int) $cartRule['reduction_currency'];
+            $cartRule['reduction_product'] = (int) $cartRule['reduction_product'];
+            $cartRule['reduction_exclude_special'] = (bool) $cartRule['reduction_exclude_special'];
+            $cartRule['gift_product'] = (int) $cartRule['gift_product'];
+            $cartRule['gift_product_attribute'] = (int) $cartRule['gift_product_attribute'];
+            $cartRule['highlight'] = (bool) $cartRule['highlight'];
+            $cartRule['active'] = (bool) $cartRule['active'];
+        }
+    }
+}
