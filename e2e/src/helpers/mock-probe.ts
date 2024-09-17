e31cb4085b29625dcdaf9f80c8dbfe18d02c96a2
John.R <jonathan_renard@hotmail.fr>
2024-09-17 10:12:58 +0200

    Reapply "WIP : work pls"
    This reverts commit 5f7906181737c9446cd1bd6c02ccb687ece62b70.

--- e2e/src/helpers/mock-probe.ts
+++ e2e/src/helpers/mock-probe.ts
@@ -1,23 +1,26 @@
-import WebSocket from 'ws';
-import {WebSocketSubject} from "rxjs/webSocket";
+import WebSocket from "ws";
+import { WebSocketSubject } from "rxjs/webSocket";
 import {
   EMPTY,
   expand,
-  filter, firstValueFrom,
+  filter,
+  firstValueFrom,
   from,
   map,
-  Observable, take,
-  tap, throwIfEmpty,
-  timeout
+  Observable,
+  take,
+  tap,
+  throwIfEmpty,
+  timeout,
 } from "rxjs";
-import R from 'ramda';
+import R from "ramda";
 import testConfig from "./test.config";
 import axios from "axios";
-import {ShopContent} from "./shop-contents";
-import {anyBoolean} from "jest-mock-extended";
+import { ShopContent } from "./shop-contents";
+import { anyBoolean } from "jest-mock-extended";
 
 const DEFAULT_OPTIONS = {
-  timeout: 3000
+  timeout: 3000,
 };
 
 export type MockProbeOptions = typeof DEFAULT_OPTIONS;
@@ -31,76 +34,95 @@ if (!global.WebSocket) {
 let wsConnection: Observable<MockProbeResponse> = null;
 function getProbeSocket(): Observable<MockProbeResponse> {
   if (!wsConnection) {
-    wsConnection = new WebSocketSubject<MockProbeResponse>('ws://localhost:8080');
+    wsConnection = new WebSocketSubject<MockProbeResponse>(
+      "ws://localhost:8080",
+    );
   }
   return wsConnection;
 }
 
-export type MockProbeResponse = {
-  apiName: string,
-  method: string,
-  headers: Record<string, string>,
-  url: string,
-  query: Record<string, string>,
-  params: Record<string, string>,
-  body: Record<string, any> & { file: any[] }
-}
-
-export function probe(match?: Partial<MockProbeResponse>, options?: MockProbeOptions): Observable<MockProbeResponse> {
+export function probe(
+  match?: Partial<MockProbeResponse>,
+  options?: MockProbeOptions,
+): Observable<MockProbeResponse> {
   options = R.mergeLeft(options, DEFAULT_OPTIONS);
   const socket = getProbeSocket();
 
   return socket.pipe(
-    filter(message => match ? R.whereEq(match, message) : true),
+    filter((message) => (match ? R.whereEq(match, message) : true)),
     timeout(options.timeout),
-  )
+  );
 }
 
 export type PsEventbusSyncResponse = {
-  job_id: string,
-  object_type: string,
-  syncType: string, // 'full' | 'incremental'
-  total_objects: number, // may not always be accurate, can't be relied on
-  has_remaining_objects: boolean, // reliable
-  remaining_objects: number, // may not always be accurate, can't be relied on
-  md5: string,
-  status: boolean,
-  httpCode: number,
-  body: unknown, // not sure what this is
-  upload_url: string,
-}
+  job_id: string;
+  object_type: string;
+  syncType: string; // 'full' | 'incremental'
+  total_objects: number; // may not always be accurate, can't be relied on
+  has_remaining_objects: boolean; // reliable
+  remaining_objects: number; // may not always be accurate, can't be relied on
+  md5: string;
+  status: boolean;
+  httpCode: number;
+  body: unknown; // not sure what this is
+  upload_url: string;
+};
 
 // TODO define collection as type literal
-export type Collection = string
+export type Collection = string;
 
 export type PsEventbusSyncUpload = {
-  collection: Collection, id: string, properties: any
-}
+  collection: Collection;
+  id: string;
+  properties: any;
+};
 
-export function doFullSync(jobId: string, shopContent: ShopContent, options?: MockClientOptions): Observable<PsEventbusSyncResponse> {
+export type MockProbeResponse = {
+  apiName: string;
+  method: string;
+  headers: Record<string, string>;
+  url: string;
+  query: Record<string, string>;
+  params: Record<string, string>;
+  body: Record<string, any> & { file: PsEventbusSyncUpload[] };
+};
+
+export function doFullSync(
+  jobId: string,
+  shopContent: ShopContent,
+  options?: MockClientOptions,
+): Observable<PsEventbusSyncResponse> {
   options = R.mergeLeft(options, DEFAULT_OPTIONS);
-  
-  const callId = { 'call_id': Math.random().toString(36).substring(2, 11) };
 
-  const requestNext = (full: number) => axios.post<PsEventbusSyncResponse>(
-    `${testConfig.prestashopUrl}/index.php?fc=module&module=ps_eventbus&controller=apiFront&is_e2e=1&shop_content=${shopContent}&limit=5&full=${full}&job_id=${jobId}`,
-    callId,
-    {
-      headers: {
-        'Host': testConfig.prestaShopHostHeader,
-        'Content-Type': 'application/x-www-form-urlencoded' // for compat PHP 5.6
-      }
-    });
+  const callId = { call_id: Math.random().toString(36).substring(2, 11) };
+
+  const requestNext = (full: number) => {
+    console.log("isFull", full === 1);
+    return axios.post<PsEventbusSyncResponse>(
+      `${testConfig.prestashopUrl}/index.php?fc=module&module=ps_eventbus&controller=apiFront&is_e2e=1&shop_content=${shopContent}&limit=5&full=${full}&job_id=${jobId}`,
+      callId,
+      {
+        headers: {
+          Host: testConfig.prestaShopHostHeader,
+          "Content-Type": "application/x-www-form-urlencoded", // for compat PHP 5.6
+        },
+      },
+    );
+  };
+
+  console.log("doFullSync");
 
   return from(requestNext(1)).pipe(
-    expand(response => {
+    expand((response) => {
+      console.log(response.data.remaining_objects + " remaining objects");
       if (response.data.has_remaining_objects) {
         return from(requestNext(0));
       } else {
-        return EMPTY
+        console.log("has empty");
+        return EMPTY;
       }
     }),
     timeout(options.timeout),
-    map(response => response.data)
-  )
+    map((response) => response.data),
+  );
 }
