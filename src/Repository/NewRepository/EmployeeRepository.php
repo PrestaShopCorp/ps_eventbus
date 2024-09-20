4a10822836825edefae208add0760fc8fe525b86
John.R <jonathan_renard@hotmail.fr>
2024-09-20 15:46:48 +0200

    feat: add employees

---
+++ src/Repository/NewRepository/EmployeeRepository.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace PrestaShop\Module\PsEventbus\Repository\NewRepository;
+
+class EmployeeRepository extends AbstractRepository implements RepositoryInterface
+{
+    const TABLE_NAME = 'employee';
+
+    /**
+     * @param string $langIso
+     * @param bool $withSelecParameters
+     *
+     * @return mixed
+     *
+     * @throws \PrestaShopException
+     */
+    public function generateFullQuery($langIso, $withSelecParameters)
+    {
+        $this->generateMinimalQuery(self::TABLE_NAME, 'e');
+
+        $this->query
+            ->leftJoin('employee_shop', 'es', 'es.id_employee = e.id_employee')
+            ->where('es.id_shop = ' . parent::getShopContext()->id)
+        ;
+
+        if ($withSelecParameters) {
+            $this->query
+                ->select('e.id_employee')
+                ->select('e.id_profile')
+                ->select('e.id_lang')
+                ->select('e.email')
+                ->select('e.bo_color')
+                ->select('e.bo_theme')
+                ->select('e.bo_css')
+                ->select('e.default_tab')
+                ->select('e.bo_width')
+                ->select('e.bo_menu')
+                ->select('e.active')
+                ->select('e.optin')
+                ->select('e.id_last_order')
+                ->select('e.id_last_customer_message')
+                ->select('e.id_last_customer')
+                ->select('e.last_connection_date')
+                ->select('es.id_shop as id_shop')
+            ;
+
+            // https://github.com/PrestaShop/PrestaShop/commit/20f1d9fe8a03559dfa9d1f7109de1f70c99f1874#diff-cde6a9d4a58afb13ff068801ee09c0e712c5e90b0cbf5632a0cc965f15cb6802R107
+            if (defined('_PS_VERSION_') && version_compare(_PS_VERSION_, '1.7.8.0', '>=')) {
+                $this->query->select('e.has_enabled_gravatar');
+            }
+        }
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     *
+     * @throws \PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     */
+    public function retrieveContentsForFull($offset, $limit, $langIso, $debug)
+    {
+        $this->generateFullQuery($langIso, true);
+
+        $this->query->limit((int) $limit, (int) $offset);
+
+        return $this->runQuery($debug);
+    }
+
+    /**
+     * @param int $limit
+     * @param array<mixed> $contentIds
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     *
+     * @throws \PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     */
+    public function retrieveContentsForIncremental($limit, $contentIds, $langIso, $debug)
+    {
+        $this->generateFullQuery($langIso, true);
+
+        $this->query
+            ->where('c.id_employee IN(' . implode(',', array_map('intval', $contentIds)) . ')')
+            ->limit($limit)
+        ;
+
+        return $this->runQuery($debug);
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @param string $langIso
+     *
+     * @return int
+     *
+     * @throws \PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     */
+    public function countFullSyncContentLeft($offset, $limit, $langIso)
+    {
+        $this->generateFullQuery($langIso, false);
+
+        $this->query->select('(COUNT(*) - ' . (int) $offset . ') as count');
+
+        $result = $this->runQuery(false);
+
+        return $result[0]['count'];
+    }
+}
---
+++ src/Service/ShopContent/EmployeesService.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace PrestaShop\Module\PsEventbus\Service\ShopContent;
+
+use PrestaShop\Module\PsEventbus\Config\Config;
+use PrestaShop\Module\PsEventbus\Repository\NewRepository\EmployeeRepository;
+
+class EmployeesService implements ShopContentServiceInterface
+{
+    /** @var EmployeeRepository */
+    private $employeeRepository;
+
+    public function __construct(EmployeeRepository $employeeRepository)
+    {
+        $this->employeeRepository = $employeeRepository;
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     */
+    public function getContentsForFull($offset, $limit, $langIso, $debug)
+    {
+        $result = $this->employeeRepository->retrieveContentsForFull($offset, $limit, $langIso, $debug);
+
+        if (empty($result)) {
+            return [];
+        }
+
+        $this->castEmployees($result);
+
+        return array_map(function ($item) {
+            return [
+                'id' => (string) $item['id_employee'],
+                'collection' => Config::COLLECTION_EMPLOYEES,
+                'properties' => $item,
+            ];
+        }, $result);
+    }
+
+    /**
+     * @param int $limit
+     * @param array<string, int> $contentIds
+     * @param string $langIso
+     * @param bool $debug
+     *
+     * @return array<mixed>
+     */
+    public function getContentsForIncremental($limit, $contentIds, $langIso, $debug)
+    {
+        $result = $this->employeeRepository->retrieveContentsForIncremental($limit, $contentIds, $langIso, $debug);
+
+        if (empty($result)) {
+            return [];
+        }
+
+        $this->castEmployees($result);
+
+        return array_map(function ($item) {
+            return [
+                'id' => (string) $item['id_employee'],
+                'collection' => Config::COLLECTION_EMPLOYEES,
+                'properties' => $item,
+            ];
+        }, $result);
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @param string $langIso
+     *
+     * @return int
+     */
+    public function getFullSyncContentLeft($offset, $limit, $langIso)
+    {
+        return $this->employeeRepository->countFullSyncContentLeft($offset, $limit, $langIso);
+    }
+
+    /**
+     * @param array<mixed> $currencies
+     *
+     * @return void
+     */
+    private function castEmployees(&$currencies)
+    {
+        foreach ($currencies as &$employee) {
+            $employee['id_employee'] = (int) $employee['id_employee'];
+            $employee['id_profile'] = (int) $employee['id_profile'];
+            $employee['id_lang'] = (int) $employee['id_lang'];
+
+            $employee['default_tab'] = (int) $employee['default_tab'];
+            $employee['bo_width'] = (int) $employee['bo_width'];
+            $employee['bo_menu'] = (bool) $employee['bo_menu'];
+
+            $employee['optin'] = (bool) $employee['optin'];
+            $employee['active'] = (bool) $employee['active'];
+
+            $employee['id_last_order'] = (int) $employee['id_last_order'];
+            $employee['id_last_customer_message'] = (int) $employee['id_last_customer_message'];
+            $employee['id_last_customer'] = (int) $employee['id_last_customer'];
+
+            if ($employee['last_connection_date'] == '0000-00-00') {
+                $employee['last_connection_date'] = null;
+            } else {
+                $employee['last_connection_date'] = (string) $employee['last_connection_date'];
+            }
+
+            $employee['id_shop'] = (int) $employee['id_shop'];
+
+            // https://github.com/PrestaShop/PrestaShop/commit/20f1d9fe8a03559dfa9d1f7109de1f70c99f1874#diff-cde6a9d4a58afb13ff068801ee09c0e712c5e90b0cbf5632a0cc965f15cb6802R107
+            if (defined('_PS_VERSION_') && version_compare(_PS_VERSION_, '1.7.8.0', '>=')) {
+                $employee['has_enabled_gravatar'] = (bool) $employee['has_enabled_gravatar'];
+            }
+
+            // FIXME : use a random salt generated during module install
+            $employee['email_hash'] = hash('sha256', $employee['email'] . 'dUj4GMBD6689pL9pyr');
+            unset($employee['email']);
+        }
+    }
+}
