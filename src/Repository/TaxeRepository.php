8697cd6cb205d16021647ab2a71201cd30627c3d
John.R <jonathan_renard@hotmail.fr>
2024-10-28 10:16:21 +0100

    fix: remove pseudo cache on request

--- src/Repository/TaxeRepository.php
+++ src/Repository/TaxeRepository.php
@@ -35,11 +35,6 @@ class TaxeRepository extends AbstractRepository
     const TABLE_NAME = 'tax';
 
     /**
-     * @var array<mixed>
-     */
-    private $countryIsoCodeCache = [];
-
-    /**
      * @param int $zoneId
      * @param int $taxRulesGroupId
      * @param bool $active
@@ -50,37 +45,31 @@ class TaxeRepository extends AbstractRepository
      */
     public function getCarrierTaxesByZone($zoneId, $taxRulesGroupId, $active)
     {
-        $cacheKey = $zoneId . '-' . (int) $active;
+        $this->generateMinimalQuery(self::TABLE_NAME, 't');
 
-        if (!isset($this->countryIsoCodeCache[$cacheKey])) {
-            $this->generateMinimalQuery(self::TABLE_NAME, 't');
+        $this->query
+            ->innerJoin('tax_rule', 'tr', 'tr.id_tax = t.id_tax')
+            ->innerJoin('tax_rules_group', 'trg', 'trg.id_tax_rules_group = tr.id_tax_rules_group')
+            ->innerJoin('tax_rules_group_shop', 'trgs', 'trgs.id_tax_rules_group = tr.id_tax_rules_group')
+            ->innerJoin('tax_lang', 'tl', 'tl.id_tax = t.id_tax')
+            ->leftJoin('country', 'c', 'c.id_country = tr.id_country')
+            ->leftJoin('state', 's', 's.id_state = tr.id_state')
+            ->where('tr.id_tax_rules_group = ' . (int) $taxRulesGroupId)
+            ->where('c.active = ' . (bool) $active)
+            ->where('s.active = ' . (bool) $active . ' OR s.active IS NULL')
+            ->where('t.active = ' . (bool) $active)
+            ->where('c.id_zone = ' . (int) $zoneId . ' OR s.id_zone = ' . (int) $zoneId)
+            ->where('c.iso_code IS NOT NULL')
+            ->where('trgs.id_shop = ' . parent::getShopContext()->id)
+            ->where('tl.id_lang = ' . (int) parent::getLanguageContext()->id)
+        ;
 
-            $this->query
-                ->innerJoin('tax_rule', 'tr', 'tr.id_tax = t.id_tax')
-                ->innerJoin('tax_rules_group', 'trg', 'trg.id_tax_rules_group = tr.id_tax_rules_group')
-                ->innerJoin('tax_rules_group_shop', 'trgs', 'trgs.id_tax_rules_group = tr.id_tax_rules_group')
-                ->innerJoin('tax_lang', 'tl', 'tl.id_tax = t.id_tax')
-                ->leftJoin('country', 'c', 'c.id_country = tr.id_country')
-                ->leftJoin('state', 's', 's.id_state = tr.id_state')
-                ->where('tr.id_tax_rules_group = ' . (int) $taxRulesGroupId)
-                ->where('c.active = ' . (bool) $active)
-                ->where('s.active = ' . (bool) $active . ' OR s.active IS NULL')
-                ->where('t.active = ' . (bool) $active)
-                ->where('c.id_zone = ' . (int) $zoneId . ' OR s.id_zone = ' . (int) $zoneId)
-                ->where('c.iso_code IS NOT NULL')
-                ->where('trgs.id_shop = ' . parent::getShopContext()->id)
-                ->where('tl.id_lang = ' . (int) parent::getLanguageContext()->id)
-            ;
+        $this->query
+            ->select('t.rate')
+            ->select('c.iso_code as country_iso_code')
+            ->select('GROUP_CONCAT(s.iso_code SEPARATOR ",") as state_iso_code')
+        ;
 
-            $this->query
-                ->select('t.rate')
-                ->select('c.iso_code as country_iso_code')
-                ->select('GROUP_CONCAT(s.iso_code SEPARATOR ",") as state_iso_code')
-            ;
-
-            $this->countryIsoCodeCache[$cacheKey] = $this->runQuery(true);
-        }
-
-        return $this->countryIsoCodeCache[$cacheKey];
+        return $this->runQuery(true);
     }
 }
