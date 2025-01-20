c45c516834b22b61e0596bdb02f3ecfbd31a5d42
John.R <jonathan_renard@hotmail.fr>
2025-01-20 10:12:48 +0100

    fix: bundles

--- src/Repository/BundleRepository.php
+++ src/Repository/BundleRepository.php
@@ -103,7 +103,7 @@ class BundleRepository extends AbstractRepository implements RepositoryInterface
 
         $this->query
             ->where('pac.id_product_pack IN(' . implode(',', array_map('intval', $contentIds)) . ')')
-            // ->limit($limit) Sub shop content depend from another, temporary disabled
+            ->limit($limit)
         ;
 
         return $this->runQuery();
