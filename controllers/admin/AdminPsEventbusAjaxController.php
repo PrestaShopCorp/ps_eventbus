e4604343744a85d5f73583b9e73db715ba0b43f2
John.R <jonathan_renard@hotmail.fr>
2026-08-12 10:53:36 +0200

    fix: check method existence before calling access in ajaxProcessDispatch

--- controllers/admin/AdminPsEventbusAjaxController.php
+++ controllers/admin/AdminPsEventbusAjaxController.php
@@ -24,7 +24,7 @@ class AdminPsEventbusAjaxController extends ModuleAdminController
      */
     public function ajaxProcessDispatch()
     {
-        if (!$this->access('view')) {
+        if (method_exists($this, 'access') && !$this->access('view')) {
             $this->jsonResponse(['error' => true, 'message' => 'Access denied'], 403);
         }
 
